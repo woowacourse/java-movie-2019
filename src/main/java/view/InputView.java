a12aa083a1bbf6c90f096d8f080254a9e5ec2861
Junmo Han <wnsah052@naver.com>
2019-04-16 18:01:57 +0900

    사용자의 입력에 해당하는 영화 선택 및 출력하기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,9 +4,34 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String INPUT_MOVIE_NUMBER_ERROR = "영화 목록에 해당되는 숫자를 입력해주세요";
+    public static final int BIRTHDAY = 1;
+    public static final int MONEY = 5;
+    public static final int FIVE_FEET = 7;
+    public static final int DUMBO = 8;
+    
+    public static int movieNumber;
 
     public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
+    	System.out.println("## 예약할 영화를 선택하세요.");
+    	do {
+    		System.out.println(INPUT_MOVIE_NUMBER_ERROR);
+    	} while(!isValidMovieNumberCondition());
+    	return movieNumber;
     }
+    
+    private static boolean isValidMovieNumberCondition() {
+    	try {
+    		String tempNumber = scanner.nextLine();
+    		movieNumber = Integer.parseInt(tempNumber);
+    		return isValidMovieNumberRange(movieNumber);
+    	} catch(NumberFormatException e) {
+    		return false;
+    	}
+    }
+    
+    private static boolean isValidMovieNumberRange(int movieNumber) {
+    	return (movieNumber == BIRTHDAY || movieNumber == MONEY
+    			|| movieNumber == FIVE_FEET || movieNumber == DUMBO ? true : false);
+    }			
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,9 +5,33 @@ import domain.Movie;
 import java.util.List;
 
 public class OutputView {
+	
+	private static int movieNum;
+	private static final int BIRTHDAY = InputView.BIRTHDAY;
+	private static final int MONEY = InputView.MONEY;
+	private static final int FIVE_FEET = InputView.FIVE_FEET;
+	private static final int FIRST_MOVIE = 0;
+	private static final int SECOND_MOVIE = 1;
+	private static final int THRID_MOVIE = 2;
+	private static final int FOURTH_MOVIE = 3;
+	
     public static void printMovies(List<Movie> movies) {
         for (Movie movie : movies) {
             System.out.println(movie);
         }
+    }
+    
+    public static void printSelectedMovies(List<Movie> movies, int movieId) {
+    	for (int i = 0; i < movies.size(); i++) {
+    	    movieNum = checkMovieNumber(movies, movieId);   
+    	}
+    	System.out.println(movies.get(movieNum));
+    }
+    
+    private static int checkMovieNumber(List<Movie> movies, int movieId) {
+    	return (movieId == BIRTHDAY ? FIRST_MOVIE
+    		: movieId == MONEY ? SECOND_MOVIE
+			: movieId == FIVE_FEET ? THRID_MOVIE
+			:                FOURTH_MOVIE);
     }
 }
