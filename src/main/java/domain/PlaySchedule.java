b5b28d0d315377fd439d4a916821c60a55862987
JINSIL <kjin_048@gmail.com>
2019-04-16 17:56:51 +0900

    Updata PlaySchedule Class 수정

--- src/main/java/domain/PlaySchedule.java
+++ src/main/java/domain/PlaySchedule.java
@@ -4,9 +4,7 @@ import java.time.LocalDateTime;
 
 import static utils.DateTimeUtils.format;
 
-public class PlaySchedule {
-    private final LocalDateTime startDateTime;
-    private int capacity;
+public class PlaySchedule { private final LocalDateTime startDateTime; private int capacity;
 
     public PlaySchedule(LocalDateTime startDateTime, int capacity) {
         this.startDateTime = startDateTime;
@@ -16,5 +14,9 @@ public class PlaySchedule {
     @Override
     public String toString() {
         return "시작시간: " + format(startDateTime) + " 예약가능인원: " + capacity + "\n";
+    }
+
+    public String getDateTime() {
+        return "시작시간: "+ format(startDateTime) ;
     }
 }
