a8bc2dc599fbb9bf3a7131a806f5bae6a1d2e5bd
LeeJoonkyu <ljk6463@gmail.com>
2019-04-16 16:23:09 +0900

    예약 인원 입력 기능 구현과 예외 처리

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -5,6 +5,7 @@ import view.InputView;
 import view.OutputView;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class MovieApplication {
@@ -21,29 +22,46 @@ public class MovieApplication {
         }
     }
 
-    public Movie movieSelection(int movieId,List<Movie> movies){
-        for (Movie movie : movies){
-            if (movie.checkMovieId(movieId)){
+    public Movie movieSelection(int movieId, List<Movie> movies) {
+        for (Movie movie : movies) {
+            if (movie.checkMovieId(movieId)) {
                 return movie;
             }
         }
         return null;
     }
 
-    int timeTableSelectionInput(Movie reservingMovie){
-        try{
+    public int timeTableSelectionInput(Movie reservingMovie) {
+        try {
             int selectedTimeTable = InputView.inputTimeTable();
             checkIfselectedTableInTimeTable(selectedTimeTable, reservingMovie);
             return selectedTimeTable;
-        } catch(IllegalArgumentException e){
+        } catch (InputMismatchException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return timeTableSelectionInput(reservingMovie);
         }
     }
 
-    public void checkIfselectedTableInTimeTable(int selectedTable, Movie reservingMovie){
-        if (selectedTable <= 0 || selectedTable >= reservingMovie.getPlayScehdules().size()){
+    public void checkIfselectedTableInTimeTable(int selectedTable, Movie reservingMovie) {
+        if (selectedTable <= 0 || selectedTable >= reservingMovie.getPlayScehdules().size()) {
             throw new IllegalArgumentException("존재 하지 않는 상영시간(시간표) 입니다. 다시 입력해주세요.");
+        }
+    }
+
+    public int reservingNumberInput(Movie reservingMovie, int selectedTable) {
+        try {
+            int reservationNumber = InputView.inputReservationNumber();
+            checkIfreservationNumberIsAvailable(reservationNumber, reservingMovie, selectedTable);
+            return reservationNumber;
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return reservingNumberInput(reservingMovie, selectedTable);
+        }
+    }
+
+    public void checkIfreservationNumberIsAvailable(int reservationNumber, Movie reservingMovie, int selectedTable) {
+        if (reservationNumber == 0 || reservingMovie.getPlayScehdules().get(selectedTable).getCapacity() < reservationNumber) {
+            throw new IllegalArgumentException("예약 인원을 입력하지 않았거나 예약 가능 인원을 초과하였습니다. 다시 입력해 주세요.");
         }
     }
 
@@ -70,12 +88,12 @@ public class MovieApplication {
         }*/
         //TODO : 바깥 루프 필요함
         m.printSelectedMovieInfo(movieId, movies);
-        Movie reservingMovie = m.movieSelection(movieId,movies);
-        selectedMovie.add(m.movieSelection(movieId,movies));
-
+        Movie reservingMovie = m.movieSelection(movieId, movies);
+        selectedMovie.add(m.movieSelection(movieId, movies));
         int selectedTable = m.timeTableSelectionInput(reservingMovie);
+        //reservingMovie 가 주요 객체다
+        int reseravationNumber = m.reservingNumberInput(reservingMovie, selectedTable);
 
-        int reseravationNumber = InputView.inputReservationNumber();
         int payOrAdditionalReservation = InputView.inputPayOrAdditionalReservation();
         //예약 내역 프린트. 이건 movie 정보를 받아야겟지
 
--- src/main/java/domain/Movie.java
+++ src/main/java/domain/Movie.java
@@ -43,4 +43,6 @@ public class Movie {
         }
         return false;
     }
+
+
 }
