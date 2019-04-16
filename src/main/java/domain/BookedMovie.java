0ef830695bb1b8689fca8721a82bf2a2e66e0b59
hjbc0921 <hjbc0921@gmail.com>
2019-04-16 15:45:53 +0900

    update BookedMovie.java : package 변경, schedule 및 예약 인원 저장 기능

---
+++ src/main/java/domain/BookedMovie.java
@@ -0,0 +1,29 @@
+package domain;
+
+import domain.Movie;
+import domain.PlaySchedule;
+
+public class BookedMovie {
+    private static final char NEW_LINE = '\n';
+    private final Movie bookedMovie;
+    private PlaySchedule selectedPlaySchedule;
+    private int reservedPersonCount;
+
+    public BookedMovie(Movie selectedMovie) {
+        bookedMovie = selectedMovie;
+    }
+
+    public void updateSchedule(PlaySchedule selectedSchedule) {
+        selectedPlaySchedule = selectedSchedule;
+    }
+
+    public void updateReservedPersonCount(int personCount) {
+        reservedPersonCount = personCount;
+    }
+
+    public void printReservation() {
+        bookedMovie.printMovieWithoutSchedule();
+        selectedPlaySchedule.printScheduleStartTime();
+        System.out.format("예약 인원 : %d명" + NEW_LINE, reservedPersonCount);
+    }
+}
