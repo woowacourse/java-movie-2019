e3564929be7f10d8edc61d101748ebbb7366a2c6
mrkwon <erased11@gmail.com>
2019-04-16 14:37:33 +0900

    InputView 클래스에 input을 받는데 필요한 메서드들 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
@@ -7,6 +8,30 @@ public class InputView {
 
     public static int inputMovieId() {
         System.out.println("## 예약할 영화를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMovieTime() {
+        System.out.println("## 예약할 시간표를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputReserveCount() {
+        System.out.println("## 예약할 인원을 입력하세요");
+        return scanner.nextInt();
+    }
+
+    public static int inputChooseProcess() {
+        System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
+        return scanner.nextInt();
+    }
+
+    public static void payProcessMessage() {
+        System.out.println("## 결제를 진행합니다.");
+    }
+
+    public static int inputPointAmount() {
+        System.out.println("## 포인트 사용 금액을 입력하세요. 포인트가 없으면 0 입력");
         return scanner.nextInt();
     }
 }
