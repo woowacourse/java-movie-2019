180c90d29d6a091a976f4e41b07c80b0a0dfe723
hjbc0921 <hjbc0921@gmail.com>
2019-04-16 14:57:40 +0900

    update OutputView.java : 안내 문구 출력 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,7 +5,10 @@ import domain.Movie;
 import java.util.List;
 
 public class OutputView {
+    private static final String ALL_MOVIES_GUIDE = "## 상영 영화 목록";
+
     public static void printMovies(List<Movie> movies) {
+        System.out.println(ALL_MOVIES_GUIDE);
         for (Movie movie : movies) {
             System.out.println(movie);
         }
