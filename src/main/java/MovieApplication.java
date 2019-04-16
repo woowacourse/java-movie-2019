2c6cc17bb3f60cd787a687b77555d084c90980ad
starkim06 <starkim06@naver.com>
2019-04-16 17:53:12 +0900

    전체 예매 진행기능 구현

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,5 +1,4 @@
-import domain.Movie;
-import domain.MovieRepository;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
@@ -7,11 +6,7 @@ import java.util.List;
 
 public class MovieApplication {
     public static void main(String[] args) {
-        List<Movie> movies = MovieRepository.getMovies();
-        OutputView.printMovies(movies);
-
-        int movieId = InputView.inputMovieId();
-
-        // TODO 구현 진행
+        ReservationManager reservationManager = new ReservationManager();
+        reservationManager.initReservation();
     }
 }
