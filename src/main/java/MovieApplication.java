ae176e0f2ec4a13f0ea1e990239ceff17db32c73
Yookibaek <psykibaek@gmail.com>
2019-04-16 17:30:51 +0900

    [ADD] [메인로직8] 예매 전체 진행 로직

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -5,11 +5,53 @@ import domain.PlaySchedule;
 import view.InputView;
 import view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieApplication {
     public static final double SALE_RATE_CARD = 0.95;
     public static final double SALE_RATE_NOCARD = 0.98;
+
+    public static void tryMakingReservation(List<MovieReservation> reservationList) {
+        // 영화 보여주기
+        List<Movie> movies = MovieRepository.getMovies();
+        OutputView.printMovies(movies);
+
+        // 예약할 영화를 선택하세요
+        int movieId = InputView.inputMovieId();
+        Movie selectedMovie = MovieRepository.getMovie(movieId);
+        OutputView.printMovie(selectedMovie);
+
+        // 스캐줄 받기
+        int scheduleIndex = InputView.inputScheduleIndex(movieId);
+
+        // 예약 인원
+        int personCount = InputView.inputReservePersonCount(movieId, scheduleIndex);
+
+        // 무비의 예약시간에 현재정보 적용해주기
+        selectedMovie.decreaseScheduleCapacity(scheduleIndex, personCount);
+        // 예약리스트에 추가하기
+        addToReservationList(reservationList, new MovieReservation(movieId, scheduleIndex, personCount));
+
+        // 예약 종료할지 더할지
+        if (InputView.inputWannaEndMovieChoice()) {
+            return;
+        }
+        tryMakingReservation(reservationList);
+    }
+
+    public static void addToReservationList(List<MovieReservation> reservationList, MovieReservation movieReservation) {
+        boolean isSameMovieExist = reservationList.stream().anyMatch(reserve -> reserve.isSameReservation(movieReservation));
+        if (isSameMovieExist) {
+            for (int i = 0; i < reservationList.size(); i++) {
+                if (reservationList.get(i).isSameReservation(movieReservation)) {
+                    reservationList.get(i).increaseCount(movieReservation.getPersonCount());
+                    return;
+                }
+            }
+        }
+        reservationList.add(movieReservation);
+    }
 
     public static void showAllReservation(List<MovieReservation> reservationList) {
         // 지금까지 예약내역 보여주기
@@ -58,11 +100,21 @@ public class MovieApplication {
     }
 
     public static void main(String[] args) {
-        List<Movie> movies = MovieRepository.getMovies();
-        OutputView.printMovies(movies);
+        List<MovieReservation> reservationList = new ArrayList<MovieReservation>();
 
-        int movieId = InputView.inputMovieId();
+        // 예약 하기
+        tryMakingReservation(reservationList);
+        // 선택된 모든 예약 보여주기
+        showAllReservation(reservationList);
+        // 모든 금액 계산하기
+        int totalMoney = getTotalPayment(reservationList);
+        // 포인트 금액 계산하기
+        int pointUseAmount = InputView.inputPoint();
+        // 신용카드 인지 현금인지
+        boolean isCardUse = InputView.inputIsCardUse();
+        // 계산하기
+        pay(totalMoney - pointUseAmount, isCardUse);
+        System.out.println("## 예매를 완료했습니다. 즐거운 영화 관람되세요.");
 
-        // TODO 구현 진행
     }
 }
