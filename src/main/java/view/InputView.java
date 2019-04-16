6325700a6ddd9a9f6c6d76e49e5920fb87d8c6a5
samsung <samsung@LAPTOP-859NPSQ7>
2019-04-16 14:53:50 +0900

    예약할 영화 번호가 정수인지 검증

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,17 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
-    }
+	public static int inputMovieId() {
+		try {
+			System.out.println("## 예약할 영화를 선택하세요.");
+			return scanner.nextInt();
+		} catch (InputMismatchException e) {
+			return inputMovieId();
+		}
+	}
 }
