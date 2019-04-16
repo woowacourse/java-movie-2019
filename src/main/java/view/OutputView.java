8575afc10fefe5c84d38be82583af33805dde15c
hyun <jhs695@naver.com>
2019-04-16 17:07:14 +0900

    Fix printMovies(), Add printPayments(), printPayResult()
    Fix
    printMovie() method를 추가함에 따라 코드의 중복 제거
    
    Add
    printPayments() : 결제정보들을 출력
    printPayResult() : 최종 결제 금액을 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,31 @@
 package view;
 
 import domain.Movie;
+import domain.PaymentInfo;
 
 import java.util.List;
 
 public class OutputView {
     public static void printMovies(List<Movie> movies) {
         for (Movie movie : movies) {
-            System.out.println(movie);
+            printMovie(movie);
         }
+    }
+
+    public static void printMovie(Movie movie) {
+        System.out.println(movie);
+    }
+
+    public static void printPayments(List<PaymentInfo> paymentInfos) {
+        for (PaymentInfo paymentInfo : paymentInfos) {
+            System.out.println(paymentInfo.getMovie().movieInfo());
+            System.out.println(paymentInfo.getPlaySchedule().getStartDateTime());
+            System.out.println("예약 인원:" + paymentInfo.getCountOfUser() + "\n");
+        }
+    }
+
+    public static void printPayResult(int money) {
+        System.out.println("최종 결제한 금액은 " + money + "원 입니다.");
+        System.out.println("예매를 완료했습니다. 즐거운 영화 관람되세요.");
     }
 }
