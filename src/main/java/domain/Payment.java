870243b6424bcea3f1e8ec259266d435b7658fde
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-16 15:51:28 +0900

    [ADD] 결제 방식, 금액을 위한 Class

---
+++ src/main/java/domain/Payment.java
@@ -0,0 +1,28 @@
+package domain;
+
+public class Payment {
+	private int price;
+	private int point;
+	private int paymentMethod;
+
+	public Payment(int price, int point) {
+		this.price = price;
+		this.point = point;
+	}
+
+	public void setPaymentMethod(int paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	// TODO enum 사용하기
+	public int calculateFinalPrice() {
+		if (paymentMethod == 1) {
+			return (int)Math.round((double)(price - point) * 0.95);
+		}
+		return (int)Math.round((double)(price - point) * 0.98);
+	}
+
+	public void printFinalPrice() {
+		System.out.println("최종 결제한 금액은 " + calculateFinalPrice() + "원 입니다.");
+	}
+}
