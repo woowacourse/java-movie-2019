af9183d679308b80f48661e631812ef592c80b42
yun <blpomspo@gmail.com>
2019-04-16 17:57:36 +0900

    영화 예매 구현
    - 영화 Id, 시간, 예매 인원 입력
    - 유효한 입력 확인
      - 예매 인원과 수용 인원 비교
      - 이전에 예매한 영화와 상영시간을 비교
      - 올바른 영화Id인지 확인
    - 예매 후 전체 예매 금액 반환

--- src/main/java/domain/ReserveMovie.java
+++ src/main/java/domain/ReserveMovie.java
@@ -9,15 +9,86 @@ import java.util.List;
 public class ReserveMovie {
     private List<Reservation> reservationList = new ArrayList<>();
 
-    public ReserveMovie() {
-
-    }
-
     public void reserve() {
-        int movieId = InputView.inputMovieId();
-        int movieTime = InputView.inputMovieTime();
-        int movieTicket = InputView.inputMovietickets();
-        reservationList.add(new Reservation(movieId, movieTime, movieTicket));
+        do {
+            reservationList.add(reserving());
+        } while (InputView.inputContinueReserve() == 2);
+        printReservationList();
     }
 
+    public void printReservationList() {
+        System.out.println("예약 내역");
+        for (Reservation re: reservationList) {
+            MovieRepository.getMovie(re.getMovieId()).printMovieInfo(re);
+            System.out.println("예약 인원: " + re.getMovieTicket());
+            System.out.println();
+        }
+    }
+
+    public Reservation reserving() {
+        int movieId;
+        int movieTime;
+        int movieTicket;
+        Reservation reservation;
+        do {
+            movieId = InputView.inputMovieId();
+            MovieRepository.printMovie(movieId);
+            movieTime = InputView.inputMovieTime();
+            movieTicket = InputView.inputMovieTicket();
+            reservation = new Reservation(movieId, movieTime, movieTicket);
+        } while (isValidReservation(reservation));
+        return reservation;
+    }
+
+    public boolean isValidReservation(Reservation reservation) {
+        return checkCapacity(reservation)
+                || !checkOverlappingTime(reservation)
+                || !checkMovieId(reservation);
+    }
+
+    /**
+     * true : 겹치는 시간 없음
+     */
+    public boolean checkOverlappingTime(Reservation reservation1) {
+        for (Reservation reservation2 : reservationList) {
+            if (MovieRepository.checkOverlapping(reservation1, reservation2)) {
+                System.out.println("현재 예약한 영화는 이전 예매한 영화 시간과 겹칩니다.");
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    /**
+     * true : 예약인원 수용 불가능
+     */
+    public boolean checkCapacity(Reservation reservation) {
+        if (MovieRepository.checkCapacity(reservation)) {
+            System.out.println("수용인원을 초과합니다.");
+            return true;
+        }
+        return false;
+    }
+
+    /** true : movieId 존재 */
+    public boolean checkMovieId(Reservation reservation) {
+        if (MovieRepository.containMovieId(reservation.getMovieId())) {
+            return true;
+        }
+        System.out.println("잘못된 영화Id입니다.");
+        return false;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        int movieId;
+        int movieTicket;
+        for (Reservation reservation: reservationList) {
+            movieId = reservation.getMovieId();
+            movieTicket = reservation.getMovieTicket();
+            totalPrice += MovieRepository.getMovie(movieId).getTicketPrice(movieTicket);
+        }
+        return totalPrice;
+    }
 }
