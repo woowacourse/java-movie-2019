381518e7d105d30783aa39d46a31c1595fb27837
hyperpace <cnk9109@gmail.com>
2019-04-16 17:09:54 +0900

    Update printPlaySchedule() to OutputView

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     private static final String TITLE_MOVIES = "##상영영화목록";
     private static final String TITLE_RESERVATIONS = "예약내역";
     private static final String TITLE_PAYMENT = "##결제를진행합니다.";
-    private static final String MESSAGE_PAYMENT_RESSULT = "최종결제한금액은 %d원입니다.\n" +
+    private static final String MESSAGE_PAYMENT_RESULT = "최종결제한금액은 %d 원입니다.\n" +
 		    "예매를완료했습니다.즐거운영화관람되세요";
 
     public static void printMovies(List<Movie> movies) {
@@ -21,6 +21,7 @@ public class OutputView {
     }
 
     public static void printPlaySchedule(Movie movie) {
+        System.out.print(movie.printSummary());
         for (PlaySchedule playSchedule : movie.getPlaySchedules()) {
             System.out.print(playSchedule.toString());
         }
@@ -33,11 +34,11 @@ public class OutputView {
         }
     }
 
-    public static void printPayment() {
+    public static void printStartPayment() {
 		System.out.println(TITLE_PAYMENT);
     }
 
     public static void printPaymentResult(int price) {
-    	System.out.println(String.format(MESSAGE_PAYMENT_RESSULT, price));
+    	System.out.println(String.format(MESSAGE_PAYMENT_RESULT, price));
     }
 }
