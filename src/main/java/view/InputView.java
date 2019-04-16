ebb167db7088da13969d799b92dadccc815b26b3
starkim06 <starkim06@naver.com>
2019-04-16 17:18:43 +0900

    오타 수정 - 사영시간 -> 상영시간

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String INPUT_MOVIE_TIME_MENT = "## 예약할 시간표를 선택하세요.(첫번째 사영 시간이 1번)";
+    private static final String INPUT_MOVIE_TIME_MENT = "## 예약할 시간표를 선택하세요.(첫번째 상영 시간이 1번)";
     private static final String INPUT_NUMBER_OF_PEOPLE_MENT = "## 예약할 인원을 입력하세요.";
     private static final String INPUT_PAY_OR_RESERVE_MENT = "## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번";
     private static final String INPUT_ERROR_MENT = "입력 형식이 올바르지 않습니다.";
