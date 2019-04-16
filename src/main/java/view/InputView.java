c765c75c62c9e81fb39f322bcf5474df8e4af48a
Sunho Van <vsh123@naver.com>
2019-04-16 18:08:51 +0900

    refactor : 오타 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -44,14 +44,14 @@ public class InputView {
     public static boolean InputContinueReservation() {
         try {
             System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
-            return chekcContinue(Integer.parseInt(scanner.nextLine().trim()));
+            return checkContinue(Integer.parseInt(scanner.nextLine().trim()));
         } catch (IllegalArgumentException e) {
             System.out.println("올바르지 않은 입력입니다.");
             return InputContinueReservation();
         }
     }
 
-    private static boolean chekcContinue(int userInput){
+    private static boolean checkContinue(int userInput){
         if(userInput == 1){
             return false;
         }
