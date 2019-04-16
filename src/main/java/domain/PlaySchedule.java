d655483c58b293503c389917a8060aab3d7bc6b7
yun <blpomspo@gmail.com>
2019-04-16 18:03:29 +0900

    - 영화 상영시간 비교

--- src/main/java/domain/PlaySchedule.java
+++ src/main/java/domain/PlaySchedule.java
@@ -1,5 +1,6 @@
 package domain;
 
+import utils.DateTimeUtils;
 import java.time.LocalDateTime;
 
 import static utils.DateTimeUtils.format;
@@ -11,6 +12,17 @@ public class PlaySchedule {
     public PlaySchedule(LocalDateTime startDateTime, int capacity) {
         this.startDateTime = startDateTime;
         this.capacity = capacity;
+    }
+
+    public static boolean compareTime(PlaySchedule schedule1,
+                                      PlaySchedule schedule2) {
+        return DateTimeUtils.isOneHourWithinRange(schedule1.startDateTime, schedule2.startDateTime);
+    }
+
+    public boolean overCapacity(Integer movieTicket) { return capacity < movieTicket; }
+
+    public void printTime() {
+        System.out.println("시작시간: " + format(startDateTime));
     }
 
     @Override
