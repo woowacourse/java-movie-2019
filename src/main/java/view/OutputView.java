4f0d7b31c5e5270a90e2737c5f4425e311d16da8
Sangwon <junothyme@gmail.com>
2019-04-16 14:24:03 +0900

    예약할 영화를 선택하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,4 +10,12 @@ public class OutputView {
             System.out.println(movie);
         }
     }
+
+    public static void showSelectedMovie(int id, List<Movie> movies) {
+        if (id <= 0 || id > movies.size()) {
+            System.out.println("숫자 범위를 넘어갔습니다. 유효한 숫자를 입력해주세요.");
+            return;
+        }
+        System.out.println(movies.get(id - 1).toString());
+    }
 }
