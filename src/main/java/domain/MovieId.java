6dd5fbd4bb9bc2f75e9de92c5c151b6b7ef95f6d
deocksoo <dusol92@snu.ac.kr>
2019-04-16 16:50:25 +0900

    [ADD] inputView 클래스의 inputMovieId 메소드를 구현합니다.
    메소드 구현에 필요한 MovieId 클래스를 추가하였습니다. MoiveId는
    movie가 movie 목록에 포함되어 있는지 여부를 검사합니다.

--- src/main/java/domain/MovieId.java
+++ src/main/java/domain/MovieId.java
@@ -1,4 +1,28 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieId {
+    private final int movieId;
+
+    private boolean isValid(int movieId) {
+        List<Movie> movies = MovieRepository.getMovies();
+        List<Integer> movieIds = new ArrayList<>();
+
+        for (Movie movie : movies) {
+            movieIds.add(movie.getId());
+        }
+
+        return movieIds.contains(movieId);
+    }
+
+    public MovieId(int movieId) {
+        if (!isValid(movieId)) {
+            System.out.println("상형 목록에 없는 영화입니다.");
+            throw new IllegalArgumentException();
+        }
+
+        this.movieId = movieId;
+    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,14 +4,22 @@ import domain.MovieId;
 import domain.NumberOfPeople;
 import domain.ScheduleNumber;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static MovieId inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return null;
+        try {
+            System.out.println("## 예약할 영화를 선택하세요.");
+            return new MovieId(Integer.parseInt(scanner.nextLine()));
+        } catch (NumberFormatException e) {
+            System.out.println("숫자만 입력할 수 있습니다.");
+            return inputMovieId();
+        } catch (IllegalArgumentException e) {
+            return inputMovieId();
+        }
     }
 
     public static ScheduleNumber inputScheduleNumber() {
