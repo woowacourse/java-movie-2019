8bb0d54d0c8c513303294152025ac6a048e8c325
qkrjh0904 <qkrjh0904@gmail.com>
2019-04-16 14:49:47 +0900

    출력될 문자열 입력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,30 @@ public class InputView {
         System.out.println("## 예약할 영화를 선택하세요.");
         return scanner.nextInt();
     }
+    
+    public static void inputMovieTime(){
+    	System.out.println("## 예약할 시간표를 선택하세요. (첫 번째 상영 시간이 1번)");
+    }
+    
+    public static void inputMovieHowMany(){
+    	System.out.println("## 예약할 인원을 입력하세요.");
+    }
+    
+    public static void inputDoneOrAdd(){
+    	System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번을 입력하세요.");
+    }
+    
+    public static void inputPoint(){
+    	System.out.println("## 포인트 사용 금액을 입력하세요. 포인트가 없으면 0 입력.");
+    }
+    
+    public static void inputCardOrCash(){
+    	System.out.println("## 신용카드는 1번, 현금은 2번을 선택하세요.");
+    }
+    
+    public static void printResult(){
+    	System.out.println("최종 결제한 금액은 " + "원 입니다.");
+    	System.out.println("예매를 완료했습니다. 즐거운 영화 관람 되세요.");
+    }
+    
 }
