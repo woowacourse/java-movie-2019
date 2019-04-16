8b17bcbc6adca827a00e67bdfc2cc2816b7f11ae
mrkwon <erased11@gmail.com>
2019-04-16 17:06:47 +0900

    예외 처리 로직 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,7 +14,7 @@ public class InputView {
             int input = Integer.parseInt(scanner.nextLine());
             return MovieRepository.getMovieById(input);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+//            System.out.println(e.getMessage());
             return inputMovieId();
         }
     }
@@ -25,27 +25,39 @@ public class InputView {
             int input = Integer.parseInt(scanner.nextLine());
             return movie.isValidTime(input);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+//            System.out.println(e.getMessage());
             return inputMovieTime(movie);
         }
     }
 
-    public static int inputReserveCount() {
+    public static int inputReserveCount(int id, Movie movie) {
+        System.out.println("## 예약할 인원을 입력하세요");
         try {
-            System.out.println("## 예약할 인원을 입력하세요");
-            return Integer.parseInt(scanner.nextLine());
+            int input = Integer.parseInt(scanner.nextLine());
+            return movie.isValidCapacity(id, input);
         } catch (IllegalArgumentException e) {
-            return inputReserveCount();
+//            System.out.println(e.getMessage());
+            return inputReserveCount(id, movie);
         }
     }
 
     public static int inputChooseProcess() {
+        System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
         try {
-            System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
-            return Integer.parseInt(scanner.nextLine());
+            int input = Integer.parseInt(scanner.nextLine());
+            isInputOneOrTwoValid(input);
+            return input;
         } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
             return inputChooseProcess();
         }
+    }
+
+    private static boolean isInputOneOrTwoValid(int input) {
+        if (input != 1 && input != 2) {
+            throw new IllegalArgumentException("유효한 입력이 아닙니다.");
+        }
+        return true;
     }
 
     public static int inputPointAmount() {
@@ -58,9 +70,11 @@ public class InputView {
     }
 
     public static int inputPayType() {
+        System.out.println("## 신용카드는 1번, 현금은 2번");
         try {
-            System.out.println("## 신용카드는 1번, 현금은 2번");
-            return Integer.parseInt(scanner.nextLine());
+            int input = Integer.parseInt(scanner.nextLine());
+            isInputOneOrTwoValid(input);
+            return input;
         } catch (IllegalArgumentException e) {
             return inputPayType();
         }
