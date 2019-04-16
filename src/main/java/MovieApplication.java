532706748da792464d31cc72e635dec0800260fe
racoco083 <uytr083@naver.com>
2019-04-16 15:21:54 +0900

    update main method

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -8,10 +8,14 @@ import java.util.List;
 public class MovieApplication {
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
+        System.out.println("## 상영 영화 목록");
         OutputView.printMovies(movies);
 
         int movieId = InputView.inputMovieId();
 
         // TODO 구현 진행
+        Manager manager = new Manager();
+        manager.startReserve(movieId);
     }
+
 }
