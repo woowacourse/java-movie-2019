636e8c04c3481cbd647686f0da46d9a990c86595
jiwon3346 <jiwon3346@gmail.com>
2019-04-16 18:00:44 +0900

    Error check

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,21 +13,21 @@ public class InputView {
 
     public static int inputMovieSchedule() {
         System.out.println("## 예약할 시간표를 선택해 주세요.");
-        return scanner.nextInt();
+        return new Scanner(System.in).nextInt();
     }
 
     public static int inputPeopleBooking() {
         System.out.println("## 예약할 인원을 입력 해주세요.");
-        return scanner.nextInt();
+        return new Scanner(System.in).nextInt();
     }
 
     public static double inputPoint() {
         System.out.println("## 포인트를 사용하시려면 입력하세요 포인트가 없으면 0 입력.");
-        return scanner.nextDouble();
+        return new Scanner(System.in).nextInt();
     }
 
     public static int inputPaymentType() {
         System.out.println("## 신용카드는 1번 현금은 2번");
-        return scanner.nextInt();
+        return new Scanner(System.in).nextInt();
     }
 }
