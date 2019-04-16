486aa02eea762c7d2607db97fdd79ccb6a356ffd
starhn87 <starhn87@gmail.com>
2019-04-16 17:42:06 +0900

    예매 조건을 통과하면 예매 가능 인원에서 예매할 인원을 뺌

--- src/main/java/domain/PlaySchedule.java
+++ src/main/java/domain/PlaySchedule.java
@@ -13,8 +13,16 @@ public class PlaySchedule {
         this.capacity = capacity;
     }
 
+    public LocalDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
     @Override
     public String toString() {
         return "시작시간: " + format(startDateTime) + " 예약가능인원: " + capacity + "\n";
+    }
+
+    public void reserve(int people) {
+        capacity = capacity - people;
     }
 }
