d2b16ecec5ebb0d286437c119ac05678229c4f4f
youngbae10000 <youngbae10000@gmail.com>
2019-04-16 16:50:35 +0900

    ADD : [기능구현] 예약을 종료하고결제를 진행1, 종료2 메세지출력, 입력 구현하다

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -3,32 +3,40 @@ import domain.MovieRepository;
 import view.InputView;
 import view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieApplication {
-    public static void main(String[] args) {
-        List<Movie> movies = MovieRepository.getMovies();
-        OutputView.printMovies(movies);
+	public static void main(String[] args) {
+		List<Movie> movies = MovieRepository.getMovies();
+		OutputView.printMovies(movies);
 
-        int movieId = InputView.inputMovieId();
+		int movieId = InputView.inputMovieId();
 
-        // TODO 구현 진행
-        
-        /*입력 받은 영화를 추가한다.*/
-        for(int i = 0; i < movies.size(); i++) {
-        	if( movieId == movies.get(i).getId()) {
-        		for(int j = 0; j < movies.get(i).getPlaySchedules().size(); j++) {
-        			System.out.print(movies.get(i).getPlaySchedules().get(j));
-        		}
-        		break;
-        	}
-        }
-        
-        /*## 예약할 시간표를 선택하세요." 메세지 출력, 
-         *   예약할 시간표를 입력받는다.*/
-        int scheduleId = InputView.inputScheduleId();
-        int reserveMenCount = InputView.inputreserveMenCount();
-        int reserveEndorContinue = InputView.inputreserveEndorContinue();
-        
-    }
+		// TODO 구현 진행
+		List<Payment> payMentList = new ArrayList<>();
+		Payment payMent = null;
+
+		/* 입력 받은 영화를 추가한다. */
+		for (int i = 0; i < movies.size(); i++) {
+			if (movieId == movies.get(i).getId()) {
+				payMent = new Payment(movies.get(i));
+				break;
+			}
+		}
+
+		/* ## 예약할 시간표를 선택하세요." 메세지 출력, 예약할 시간표를 입력받는다. */
+		int scheduleId = InputView.inputScheduleId();
+		int reserveMenCount = InputView.inputreserveMenCount();
+		int reserveEndorContinue = InputView.inputreserveEndorContinue();
+
+		/* 결제를 진행한다 */
+		if (reserveEndorContinue == 1) {
+			payMent.setReserveMenCount(reserveMenCount);
+			payMent.setScheduleId(scheduleId);
+			payMent.printPayment();
+			payMentList.add(payMent);
+		}
+
+	}
 }
