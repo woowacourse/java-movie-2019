de1e162ca001143a68b666aa919a31db671d98d7
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-04-16 16:03:23 +0900

    feat application interface : add schedule number & number to reserve

--- src/main/java/managers/ReservationManager.java
+++ src/main/java/managers/ReservationManager.java
@@ -6,22 +6,23 @@ import domain.ReservationRepository;
 import view.InputView;
 import view.OutputView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationManager {
-    public static void makeReservation(){
+    public static void makeReservation() {
         printMovieList();
-        int movieId = InputView.inputMovieId();
+        InputView.inputMovieId();
+        InputView.inputScheduleNo();
+        InputView.inputNumToReserve();
     }
 
-    private static void printMovieList(){
+    private static void printMovieList() {
         System.out.println("## 상영 영화 목록");
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
     }
 
-    public static void printReservationList(){
+    public static void printReservationList() {
         List<Movie> movies = ReservationRepository.getList();
         OutputView.printMovies(movies);
     }
