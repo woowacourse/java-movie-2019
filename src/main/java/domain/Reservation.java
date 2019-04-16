2d9c2ec6a73519b7123b34623c8bdedbd6751853
yun <blpomspo@gmail.com>
2019-04-16 14:53:17 +0900

    단일 예매 정보 저장

---
+++ src/main/java/domain/Reservation.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Reservation {
+    private int movieId;
+    private int movieTime;
+    private int movieTicket;
+
+    public Reservation(int movieId, int movieTime, int movieTicket) {
+        this.movieId = movieId;
+        this.movieTime = movieTime;
+        this.movieTicket = movieTicket;
+    }
+
+
+}
