18568cdba1e3e8791a096e0b8b72791603259e37
Jinho <wootechco.temp@gmail.com>
2019-04-16 17:41:27 +0900

    Ticket 클래스 완성

---
+++ src/main/java/domain/Ticket.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Ticket {
+    public int movieId;
+    public int time;
+    public int cnt;
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+}
