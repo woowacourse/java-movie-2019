f9a17ec69aba93afd3510128c87abdef45cb2b9c
mrkwon <erased11@gmail.com>
2019-04-16 16:03:06 +0900

    printPayResult 메서드 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -23,4 +23,9 @@ public class OutputView {
     public static void printProcessMessage() {
         System.out.println("## 결제를 진행합니다.");
     }
+
+    public static void printPayResult(int payAmount) {
+        System.out.printf("최종 결제한 금액은 %d원 입니다.\n", payAmount);
+        System.out.println("예매를 완료했습니다. 즐거운 영화 관람되세요.");
+    }
 }
