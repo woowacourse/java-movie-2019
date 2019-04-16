db5333bf979b91fe46f322c56ac80a9d05a53ff2
yk1028 <wongeunsong94@gmail.com>
2019-04-16 15:36:59 +0900

    test: MovieRepository 클래스의 hasMovie() 메소드 테스트

---
+++ src/test/java/domain/MovieRepositoryTest.java
@@ -0,0 +1,24 @@
+/*
+ * MovieRepositoryTest
+ * 
+ * version 1.0
+ * 
+ * 2019. 4. 16
+ * 
+ * Created by Wongeun Song
+ */
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import domain.MovieRepository;
+
+public class MovieRepositoryTest {
+	@Test
+	public void hasMovie() {
+		assertThat(MovieRepository.hasMovie(1, 0)).isEqualTo(true);
+		assertThat(MovieRepository.hasMovie(2, 0)).isEqualTo(false);
+	}
+}
