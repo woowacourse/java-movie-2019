dfbb43f87a9eb527c71633bea668bb28e552e85e
KyungDong_Kwon <fancyartisan@gmail.com>
2019-04-16 14:56:01 +0900

    Update MovieApplication.java Add printSelectMovie(int movieId) method - 영화 선택 기능 추가.

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -6,12 +6,28 @@ import view.OutputView;
 import java.util.List;
 
 public class MovieApplication {
+
+
+    public static void printSelectMovie(int movieId) {
+        Movie temp = MovieRepository.getSelectMovies(movieId);
+        if (temp == null) {
+            System.out.println("해당 영화가 없습니다.");
+        } else {
+            System.out.println(temp);
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println("##상영 영화 목록");
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
 
         int movieId = InputView.inputMovieId();
-
+        printSelectMovie(movieId);
         // TODO 구현 진행
     }
+
+
 }
+
+
