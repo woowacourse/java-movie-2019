b56052143adc4100338f8062d3adcea747521c44
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-04-16 14:25:11 +0900

    상영 영화 목록 출력 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,7 +5,10 @@ import domain.Movie;
 import java.util.List;
 
 public class OutputView {
+	private static final String PRINT_MOVIES_MESSAGE = "## 상영 영화 목록";
+	
     public static void printMovies(List<Movie> movies) {
+    	System.out.println(PRINT_MOVIES_MESSAGE);
         for (Movie movie : movies) {
             System.out.println(movie);
         }
