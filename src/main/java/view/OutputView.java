a9ff7aa61bc3555645c96a09699c762f0d3267fc
hyperpace <cnk9109@gmail.com>
2019-04-16 16:05:14 +0900

    Add printPlaySchedule(), printResevations(), printPayment(), printPaymentResult() to OutPutView

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,43 @@
 package view;
 
 import domain.Movie;
+import domain.PlaySchedule;
+import domain.Reservation;
 
 import java.util.List;
 
 public class OutputView {
+    private static final String TITLE_MOVIES = "##상영영화목록";
+    private static final String TITLE_RESERVATIONS = "예약내역";
+    private static final String TITLE_PAYMENT = "##결제를진행합니다.";
+    private static final String MESSAGE_PAYMENT_RESSULT = "최종결제한금액은 %d원입니다.\n" +
+		    "예매를완료했습니다.즐거운영화관람되세요";
+
     public static void printMovies(List<Movie> movies) {
+        System.out.println(TITLE_MOVIES);
         for (Movie movie : movies) {
             System.out.println(movie);
         }
+    }
+
+    public static void printPlaySchedule(Movie movie) {
+        for (PlaySchedule playSchedule : movie.getPlaySchedules()) {
+            System.out.print(playSchedule.toString());
+        }
+    }
+
+    public static void printResevations(List<Reservation> reservations) {
+        System.out.println(TITLE_RESERVATIONS);
+        for (Reservation reservation : reservations) {
+            System.out.println(reservation);
+        }
+    }
+
+    public static void printPayment() {
+		System.out.println(TITLE_PAYMENT);
+    }
+
+    public static void printPaymentResult(int price) {
+    	System.out.println(String.format(MESSAGE_PAYMENT_RESSULT, price));
     }
 }
