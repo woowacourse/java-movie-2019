df60f56ba76d62b197933d1f20ff8d33fba7f98f
youngbae10000 <youngbae10000@gmail.com>
2019-04-16 16:53:45 +0900

    ADD : [기능구현] 결제내역 리스트와 클래스를 만들다

---
+++ src/main/java/Payment.java
@@ -0,0 +1,45 @@
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+import domain.Movie;
+import view.InputView;
+
+public class Payment {
+
+	private Movie movie;
+	private int scheduleId;
+	private int reserveMenCount;
+
+	public int getScheduleId() {
+		return scheduleId;
+	}
+
+	public void setScheduleId(int scheduleId) {
+		this.scheduleId = scheduleId;
+	}
+
+	public Payment(Movie movie) {
+
+		this.movie = movie;
+
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public int getReserveMenCount() {
+		return reserveMenCount;
+	}
+
+	public void setReserveMenCount(int reserveMenCount) {
+		this.reserveMenCount = reserveMenCount;
+	}
+
+	public void printPayment() {
+		System.out.println("예약 내역");
+		System.out.println(movie.getId() + "-" + movie.getName() + "," + movie.getPrice());
+		System.out.println(movie.getPlaySchedules().get(scheduleId-1));
+		System.out.println("예약인원 : " + reserveMenCount);
+	}
+
+}
