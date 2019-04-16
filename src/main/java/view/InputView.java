d9539edf5cc88486fb9f038d7b3af4d0a9a86c88
NotYet <wiseoldster@gmail.com>
2019-04-16 16:02:45 +0900

    기능 구현 : 입력시 메세지 출력 기능들 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,28 @@ public class InputView {
         System.out.println("## 예약할 영화를 선택하세요.");
         return scanner.nextInt();
     }
+    
+    public static int inputTime() {
+        System.out.println("## 예약할 시간표를 선택하세요. (첫번째 상영 시간이 1번)");
+        return scanner.nextInt();
+    }
+    
+    public static int inputNumOfPeople() {
+        System.out.println("## 예약할 인원을 입력하세요.");
+        return scanner.nextInt();
+    }
+    
+    public static int inputCase() {
+        System.out.println("## 예약을 종료하고 결재를 진행하려면 1번, 추가 예약을 진행하려면 2번을 입력하세요.");
+        int ret = scanner.nextInt();
+        if(ret != 1 && ret != 2) {
+        	throw new IllegalArgumentException("잘못된 입력입니다. \n다시입력해주세요.");
+        }
+		return ret;
+    }
+    
+    public static int inputPoint() {
+        System.out.println("## 포인트 사용 금액을 입력해주세요. 없으면 0을 입력해주세요.");
+        return scanner.nextInt();
+    }
 }
