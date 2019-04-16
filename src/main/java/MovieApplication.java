df7d86dc91eef907c58e887b7b04b586f1f3b068
youngbae10000 <youngbae10000@gmail.com>
2019-04-16 16:56:33 +0900

    ADD :: [기능구현] 2번을 선택해 추가 예약이 진행하는 사이클을 만들다.

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -11,31 +11,38 @@ public class MovieApplication {
 		List<Movie> movies = MovieRepository.getMovies();
 		OutputView.printMovies(movies);
 
-		int movieId = InputView.inputMovieId();
+		while (true) {
 
-		// TODO 구현 진행
-		List<Payment> payMentList = new ArrayList<>();
-		Payment payMent = null;
+			int movieId = InputView.inputMovieId();
 
-		/* 입력 받은 영화를 추가한다. */
-		for (int i = 0; i < movies.size(); i++) {
-			if (movieId == movies.get(i).getId()) {
-				payMent = new Payment(movies.get(i));
-				break;
+			// TODO 구현 진행
+			List<Payment> payMentList = new ArrayList<>();
+			Payment payMent = null;
+
+			/* 입력 받은 영화를 추가한다. */
+			for (int i = 0; i < movies.size(); i++) {
+				if (movieId == movies.get(i).getId()) {
+					payMent = new Payment(movies.get(i));
+					break;
+				}
 			}
-		}
 
-		/* ## 예약할 시간표를 선택하세요." 메세지 출력, 예약할 시간표를 입력받는다. */
-		int scheduleId = InputView.inputScheduleId();
-		int reserveMenCount = InputView.inputreserveMenCount();
-		int reserveEndorContinue = InputView.inputreserveEndorContinue();
+			/* ## 예약할 시간표를 선택하세요." 메세지 출력, 예약할 시간표를 입력받는다. */
+			int scheduleId = InputView.inputScheduleId();
+			int reserveMenCount = InputView.inputreserveMenCount();
+			int reserveEndorContinue = InputView.inputreserveEndorContinue();
 
-		/* 결제를 진행한다 */
-		if (reserveEndorContinue == 1) {
-			payMent.setReserveMenCount(reserveMenCount);
-			payMent.setScheduleId(scheduleId);
-			payMent.printPayment();
-			payMentList.add(payMent);
+			/* 결제를 진행한다 */
+			if (reserveEndorContinue == 1) {
+				payMent.setReserveMenCount(reserveMenCount);
+				payMent.setScheduleId(scheduleId);
+				payMent.printPayment();
+				payMentList.add(payMent);
+			}
+
+			/* 2번을 선택할 경우 추가 예약이 진행 되도록 사이클 */
+			continue;
+
 		}
 
 	}
