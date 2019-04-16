f2e60bff5fca4004f01cdc4878c03451fd7afc79
Unknown <9105lgm@naver.com>
2019-04-16 17:11:02 +0900

    예약할 시간표 입력 , 인원 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,29 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
+        try{
+            System.out.println("## 예약할 영화를 선택하세요.");
+            return Integer.parseInt(scanner.nextLine());
+        }catch (IllegalArgumentException e){
+            return inputMovieId();
+        }
+    }
+
+    public static int inputScheduleId(){
+        try{
+            System.out.println("## 예약할 시간표를 선택하세요. (첫번째 상영 시간이 1번)");
+            return Integer.parseInt(scanner.nextLine());
+        }catch (IllegalArgumentException e){
+            return inputScheduleId();
+        }
+    }
+
+    public static int inputPerson(){
+        try{
+            System.out.println("## 예약할 인원을 입력하세요.");
+            return Integer.parseInt(scanner.nextLine());
+        }catch (IllegalArgumentException e){
+            return inputPerson();
+        }
     }
 }
