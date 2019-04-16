36c0ede27b03c3805b7ff68af927858a9538ffe5
DGpress <bgbg9655@naver.com>
2019-04-16 16:36:26 +0900

    Update BookControl.java
    프로그램 실행에 필요한 함수 정의

--- src/main/java/BookControl.java
+++ src/main/java/BookControl.java
@@ -1,3 +1,31 @@
+import domain.Movie;
+
+import java.util.List;
+
 public class BookControl {
-    
+    private List<Movie> movies; // MovieRepository로부터 가져온 영화 목록 저장 변수
+
+    public BookControl(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    // #1 영화를 예약하는 함수
+    public void bookMovie() {
+
+    }
+
+    // 예매를 더 할지 결제로 넘어갈지 판단하는 함수
+    public boolean checkMoreBook() {
+        return false;
+    }
+
+    // #2 예약된 목록을 출력하는 함수
+    public void printBookedMovies() {
+
+    }
+
+    // #3 결제를 진행할 함수
+    public void purchaseMovieTickets() {
+
+    }
 }
