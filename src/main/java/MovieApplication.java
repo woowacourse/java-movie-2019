0e9abb7341037776e3deda23ab9c4e189f9483b3
molt3nrock <molt3nrock@github.com>
2019-04-16 18:09:24 +0900

    main logic

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -9,21 +9,33 @@ import view.OutputView;
 public class MovieApplication {
 
     public static void main(String[] args) {
-        List<Movie> movies = MovieRepository.getMovies();
-        OutputView.printMovies(movies);
-
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        ReservationInputValidator inputValidator = new ReservationInputValidator();
-        List<Integer> movieNumbers = inputValidator.getReservationMovieNumbers(line);
-        ReservationPlanner reservationPlanner = new ReservationPlanner();
-        for (Integer movieNumber : movieNumbers) {
-            reservationPlanner.displayMovieSchedule(movieNumber);
-            Integer movieEntry = scanner.nextInt();
-            Integer capacity = scanner.nextInt();
-            reservationPlanner.selectReservationEntry(movieNumber, movieEntry);
-            reservationPlanner.selectReservationCapacity(movieNumber, movieEntry, capacity);
-        }
+        ReservationPlanner reservationPlanner;
+        do {
+            List<Movie> movies = MovieRepository.getMovies();
+            OutputView.printMovies(movies);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("## 예약할 영화를 선택하세요.");
+            String line = scanner.nextLine();
+            ReservationInputValidator inputValidator = new ReservationInputValidator();
+            List<Integer> movieNumbers = inputValidator.getReservationMovieNumbers(line);
+            reservationPlanner = new ReservationPlanner();
+            for (Integer movieNumber : movieNumbers) {
+                reservationPlanner.displayMovieSchedule(movieNumber);
+                System.out.println("## 예약할 시간표를 선택하세요. (첫번째 상영 시간이 1번)");
+                Integer movieEntry = scanner.nextInt();
+                System.out.println("## 예약할 인원을 입력하세요.");
+                Integer capacity = scanner.nextInt();
+                reservationPlanner.selectReservationEntry(movieNumber, movieEntry);
+                reservationPlanner.selectReservationCapacity(movieNumber, movieEntry, capacity);
+            }
+        } while (cont());
         reservationPlanner.getPlans().forEach(System.out::println);
     }
+
+    private static boolean cont() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
+        return scanner.nextInt() == 1;
+    }
+
 }
--- src/main/java/domain/ReservationRecord.java
+++ src/main/java/domain/ReservationRecord.java
@@ -1,13 +1,14 @@
 package domain;
 
 import java.time.LocalDateTime;
+import utils.DateTimeUtils;
 
 public class ReservationRecord {
 
-    public Movie movie;
+    private Movie movie;
     public int movieEntryNumber;
-    public int movieCapacity;
-    public LocalDateTime movieStartDateTime;
+    private int movieCapacity;
+    private LocalDateTime movieStartDateTime;
 
     ReservationRecord(int movieId, int movieCapacity, int movieEntryNumber) {
         Movie movie = MovieRepository.getMovies().get(movieId);
@@ -19,6 +20,12 @@ public class ReservationRecord {
 
     LocalDateTime getMovieStartDateTime() {
         return this.movieStartDateTime;
+    }
+
+    @Override
+    public String toString() {
+        String formattedDate = DateTimeUtils.format(movieStartDateTime);
+        return String.format("%s\n 시작시간 %s\n예약인원: %d", movie, formattedDate, movieCapacity);
     }
 }
 
