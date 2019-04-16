e975a8b6bfaeff5aad8b863c218bf4dbe02c1cbf
mrkwon <erased11@gmail.com>
2019-04-16 15:40:51 +0900

    예약 정보를 저장하기 위한 클래스, 예약 정보를 통해 총 결제 금액을 return 하는 기능 구현;

--- src/main/java/domain/ReservationList.java
+++ src/main/java/domain/ReservationList.java
@@ -1,4 +1,22 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReservationList {
+    private Map<Movie, Integer> reservationList = new HashMap<>();
+
+    public void add(Movie movie, Integer num) {
+        reservationList.put(movie, num);
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (Movie movie : reservationList.keySet()) {
+            int eachPrice = movie.getPrice() * reservationList.get(movie);
+            totalPrice += eachPrice;
+        }
+        return totalPrice;
+    }
+
 }
