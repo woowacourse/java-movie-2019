4bafdab27a097b0c50116ee01fbaeb4baa5ff8a7
LeeJoonkyu <ljk6463@gmail.com>
2019-04-16 17:10:50 +0900

    추가 예약 시 한 시간 이내에 영화를 예매한다면 다시 예약 하는 기능 구현

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,9 +1,12 @@
 import domain.Movie;
 import domain.MovieRepository;
+import utils.DateTimeUtils;
 import view.InputView;
 import view.OutputView;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.List;
 
@@ -11,6 +14,8 @@ public class MovieApplication {
 
     private static final int RESERVATION_FINISHED = 1;
     private static final int ADDITIONAL_RESERVATION = 2;
+    private static final int MIN = 0;
+    private static final int CONSTANT_TO_MAKE_INDEX = 1;
 
     public void printSelectedMovieInfo(int movieId, List<Movie> movies) {
         for (Movie movie : movies) {
@@ -45,7 +50,7 @@ public class MovieApplication {
     }
 
     public void checkIfselectedTableInTimeTable(int selectedTable, Movie reservingMovie) {
-        if (selectedTable <= 0 || selectedTable >= reservingMovie.getPlayScehdules().size()) {
+        if (selectedTable <= MIN || selectedTable > reservingMovie.getPlayScehdules().size()) {
             throw new IllegalArgumentException("존재 하지 않는 상영시간(시간표) 입니다. 다시 입력해주세요.");
         }
     }
@@ -62,74 +67,91 @@ public class MovieApplication {
     }
 
     public void checkIfreservationNumberIsAvailable(int reservationNumber, Movie reservingMovie, int selectedTable) {
-        if (reservationNumber == 0 || reservingMovie.getPlayScehdules().get(selectedTable).getCapacity() < reservationNumber) {
+        if (reservationNumber == MIN || reservingMovie.getPlayScehdules().get(selectedTable).getCapacity() < reservationNumber) {
             throw new IllegalArgumentException("예약 인원을 입력하지 않았거나 예약 가능 인원을 초과하였습니다. 다시 입력해 주세요.");
         }
     }
 
-    public int payOrAdditionalReservationInput(){
+    public int payOrAdditionalReservationInput() {
         try {
             int payOrReservation = InputView.inputPayOrAdditionalReservation();
             checkIfPayOrReservationIsProper(payOrReservation);
             return payOrReservation;
 
-        } catch(InputMismatchException | IllegalArgumentException e){
+        } catch (InputMismatchException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return payOrAdditionalReservationInput();
         }
     }
 
-    public void checkIfPayOrReservationIsProper(int payOrReservation){
-        if (payOrReservation < RESERVATION_FINISHED || payOrReservation > ADDITIONAL_RESERVATION){
+    public void checkIfPayOrReservationIsProper(int payOrReservation) {
+        if (payOrReservation < RESERVATION_FINISHED || payOrReservation > ADDITIONAL_RESERVATION) {
             throw new IllegalArgumentException("1 또는 2의 숫자를 입력해주세요.");
         }
     }
 
+    public boolean checkIfReservationEnd(int payOrAdditionalReservation) {
+        if (payOrAdditionalReservation == ADDITIONAL_RESERVATION) {
+            //TODO : 위로 돌아감
+            return false;
+        }
+        return true;
+    }
+
+    public void printReservationList(List<Movie> reservedMovie, int selectedTable, int reservationNumber) {
+
+
+    }
+
+    public boolean checkIfOneHourWithinRange(List<Movie> reservedMovie, LocalDateTime dateTimetoCheck, int selectedTable) {
+        boolean isOneHourWithinRange = false;
+        for (Movie movie : reservedMovie){
+            LocalDateTime movieTimeToCheck = movie.getPlayScehdules().get(selectedTable).getStartDateTime();
+            DateTimeUtils.isOneHourWithinRange(movieTimeToCheck, dateTimetoCheck);
+        }
+        return isOneHourWithinRange;
+
+    }
+
+
+    public void getStartingHours
 
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
 
-        int movieId = InputView.inputMovieId();
-
         MovieApplication m = new MovieApplication();
         List<Movie> reservedMovie = new ArrayList<>();
-        //movie List 를 가져와서 파싱하면서 해결해야함.
-        //ID를 받아서,
 
-        //MovieRepository.printMovieInfo(movieId);
-        //Id 에 맞는 무비 정보를 가져온다.
-        //해당 무비의 info를 출력한다.
-        //해당 무비의 플레이스케쥴을, 새로운 List에 추가한다.
-        //해당 리스트를 돌면서,
-        /*for (Movie movie : movies){
-            movie.getPlayScehdules().get[selectedTable];
-            for (PlaySchedule playSchedule : movie.)
-        }*/
+        boolean isReservationEnd = false;
+        boolean isOneHourWithinRange = false;
         //TODO : 바깥 루프 필요함
-        m.printSelectedMovieInfo(movieId, movies);
-        Movie reservingMovie = m.movieSelection(movieId, movies);
-        reservedMovie.add(m.movieSelection(movieId, movies));
-        int selectedTable = m.timeTableSelectionInput(reservingMovie);
-        //reservingMovie 가 주요 객체다
-        int reseravationNumber = m.reservingNumberInput(reservingMovie, selectedTable);
-        int payOrAdditionalReservation = m.payOrAdditionalReservationInput();
+        while (!isReservationEnd) {
+            int movieId = InputView.inputMovieId();
+            m.printSelectedMovieInfo(movieId, movies);
+            Movie movieReservingNow = m.movieSelection(movieId, movies);
+            reservedMovie.add(m.movieSelection(movieId, movies));
+            int selectedTable = m.timeTableSelectionInput(movieReservingNow);
+
+            LocalDateTime dateTimeToCheck = movieReservingNow.getPlayScehdules().get(selectedTable).getStartDateTime();
+            isOneHourWithinRange = m.checkIfOneHourWithinRange(reservedMovie, dateTimeToCheck, selectedTable);
+            if (isOneHourWithinRange) {
+                System.out.println("상영 시간이 예매된 영화와 1시간 이내입니다. 다시 예약 해주세요.");
+                continue;
+            }
+            int reseravationNumber = m.reservingNumberInput(movieReservingNow, selectedTable);
 
 
-        if (payOrAdditionalReservation == ADDITIONAL_RESERVATION){
-            //TODO : 위로 돌아감
+            int payOrAdditionalReservation = m.payOrAdditionalReservationInput();
+            isReservationEnd = m.checkIfReservationEnd(payOrAdditionalReservation);
         }
-        if (payOrAdditionalReservation == RESERVATION_FINISHED){
-            //printReservationList(reservedMovie);
-            InputView.printPaymentRunningMessage();
-            int amountOfPointToUse = InputView.inputAmountOfPointToUse();
-            int payByCreditOrCash = InputView.inputPayByCreditCardOrCash();
-        }
-
+        //예약이 끝낫다면,
+        printReservationList(reservedMovie);
+        InputView.printPaymentRunningMessage();
+        int amountOfPointToUse = InputView.inputAmountOfPointToUse();
+        int payByCreditOrCash = InputView.inputPayByCreditCardOrCash();
 
         //예약 내역 프린트. 이건 movie 정보를 받아야겟지
-
-
 
 
     }
