8235abb2dcae12f216e2876f7874f47160c44dc5
seongchanchoi <chsch911028@gmail.com>
2019-04-16 15:08:56 +0900

    ADD FUNCTION : [사용자 입력] 예약할 영화 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    private static final String SELECT_MOVIE_TO_RESERVE_MESSAGE = "## 예약할 영화를 선택하세요.";
+
     public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
+        System.out.println(SELECT_MOVIE_TO_RESERVE_MESSAGE);
         return scanner.nextInt();
     }
 }
