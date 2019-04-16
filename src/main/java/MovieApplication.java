1d2c478dedad0abcebc4aeb8f7a825938e1ba73e
mrkwon <erased11@gmail.com>
2019-04-16 16:24:12 +0900

    메인 로직 수정

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -10,26 +10,26 @@ import java.util.List;
 public class MovieApplication {
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
-        OutputView.printMovies(movies);
-
-        int movieId = InputView.inputMovieId();
-
-        // TODO 구현 진행
-        // 영화 선택
-        Movie aMovie = MovieRepository.getMovieById(movieId);
-        OutputView.printAMovie(aMovie);
-
         ReservationList reservationList = new ReservationList();
 
+        OutputView.printMovies(movies);
+
+        // TODO 구현 진행
         int process;
         do {
+            int movieId = InputView.inputMovieId();
+
+            Movie aMovie = MovieRepository.getMovieById(movieId);
+            OutputView.printAMovie(aMovie);
+
             int movieTime = InputView.inputMovieTime();
             int reserveCount = InputView.inputReserveCount();
 
             process = InputView.inputChooseProcess();
-            OutputView.printReserveList(aMovie, movieTime, reserveCount);
-            reservationList.add(aMovie, reserveCount);
+            reservationList.add(aMovie, reserveCount, movieTime);
         } while (process == 2);
+
+        OutputView.printReserveList(reservationList);
 
         OutputView.printProcessMessage();
         int pointAmount = InputView.inputPointAmount();
