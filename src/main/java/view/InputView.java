2ec8e55840928f78cf32914fd0e139b40dac6493
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-16 15:39:09 +0900

    [MOD] 안내 메세지 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -34,6 +34,7 @@ public class InputView {
 	
 	public static int inputPoint() {
 		try {
+			System.out.println("## 결제를 진행합니다.");
 			System.out.println("## 포인트 사용 금액을 입력하세요. 포인트가 없으면 0 입력");
 			return Integer.parseInt(SCANNER.nextLine());
 		} catch (IllegalArgumentException e) {
