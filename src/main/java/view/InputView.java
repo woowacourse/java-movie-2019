c7e25739ad2fa23167b361bbd3ff315cbf720823
moonsung <visitant5@nate.com>
2019-04-16 14:51:34 +0900

    update InputView

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,30 @@ public class InputView {
         System.out.println("## 예약할 영화를 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputMovieTime() {
+        System.out.println("## 예약할 시간을 선택하세요. (첫번째 상영 시간이 1번)");
+        return scanner.nextInt();
+    }
+
+    public static int inputMovieNum() {
+        System.out.println("## 예약할 인원을 입력하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int InputNextStep() {
+        System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
+        return scanner.nextInt();
+    }
+
+    public static int inputMoviePoint() {
+        System.out.println("## 포인트 사용금액을 입력하세요. 포인트가 없으면 0 입력");
+        return scanner.nextInt();
+    }
+
+    public static int inputCardorCash() {
+        System.out.println("## 신용카드는 1번, 현금은 2번");
+        return scanner.nextInt();
+    }
+
 }
