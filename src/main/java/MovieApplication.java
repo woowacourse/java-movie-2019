0b0c8e668a6a033abcf559098fd484d5d206d3e0
Jinho <wootechco.temp@gmail.com>
2019-04-16 17:45:06 +0900

    입력 및 출력 완성

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,17 +1,106 @@
-import domain.Movie;
+import domain.CGV;
+import domain.Customer;
 import domain.MovieRepository;
+import domain.Ticket;
 import view.InputView;
 import view.OutputView;
 
-import java.util.List;
-
 public class MovieApplication {
+    private static CGV theater = new CGV();
+    private static Customer customer = new Customer(0);
+
     public static void main(String[] args) {
-        List<Movie> movies = MovieRepository.getMovies();
-        OutputView.printMovies(movies);
+        theater.setAbleMovies(MovieRepository.getMovies());
 
-        int movieId = InputView.inputMovieId();
+        while (true) {
+            OutputView.printMovies(theater.getAbleMovies());
 
-        // TODO 구현 진행
+            Ticket ticket = new Ticket();
+
+            // 영화 선택
+            ticket = selectMovies(ticket);
+
+            // 예약 시간 선택
+            ticket = selectTimes(ticket);
+
+            // 예약할 인원 입력
+            ticket = inputCnt(ticket);
+
+            // 예약한 상태로
+            customer.reserve(ticket);
+            theater.lock(customer);
+
+            // 추가 예약
+            if (!InputView.inputExit()) {
+                continue;
+            }
+            break;
+        }
+
+        // 확인, 예약 내역 출력
+        theater.confirm(customer);
+
+        int point;
+        while (true) {
+            point = InputView.inputPoint();
+            if (customer.getPoint() < point) {
+                System.out.println("포인트가 부족합니다. 다시 입력해 주세요.");
+                continue;
+            }
+            break;
+        }
+
+        int way;
+        while (true) {
+            way = InputView.inputWay();
+            if (way != CGV.CARD && way != CGV.CASH) {
+                System.out.println("결제 방법을 다시 입력해주세요.");
+                continue;
+            }
+            break;
+        }
+
+        // 최종 구매
+        theater.buy(customer, point, way);
+    }
+
+    public static Ticket selectMovies(Ticket ticket) {
+        while (true) {
+            ticket.setMovieId(InputView.inputMovieId());
+            CGV.ReserveMsg movieMsg = theater.checkMoveNumber(ticket);
+            if (movieMsg != CGV.ReserveMsg.NONE) {
+                System.out.println(movieMsg.getMsg());
+                continue;
+            }
+            break;
+        }
+        OutputView.printSchedule(theater.getMovie(ticket.movieId));
+        return ticket;
+    }
+
+    public static Ticket selectTimes(Ticket ticket) {
+        while (true) {
+            ticket.setTime(InputView.inputTime() - 1);
+            CGV.ReserveMsg timeMsg = theater.checkTime(customer.getReserveList(), ticket);
+            if (timeMsg != CGV.ReserveMsg.NONE) {
+                System.out.println(timeMsg.getMsg());
+                continue;
+            }
+            break;
+        }
+        return ticket;
+    }
+
+    public static Ticket inputCnt(Ticket ticket) {
+        while (true) {
+            ticket.setCnt(InputView.inputCnt());
+            CGV.ReserveMsg cntMsg = theater.checkCnt(ticket);
+            if (cntMsg != CGV.ReserveMsg.NONE) {
+                System.out.println(cntMsg.getMsg());
+                continue;
+            }
+            break;
+        }
+        return ticket;
     }
 }
