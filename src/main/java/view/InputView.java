e8044a8a7bcafbb16f1362c782581109b5234ff1
Soojin Roh <sooreal91@gmail.com>
2019-04-16 14:48:40 +0900

    예약할 인원 입력 안내 메시지 출력 함수 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,4 +14,9 @@ public class InputView {
     	System.out.println("## 예약할 시간표를 선택하세요. (첫 번째 상영 시간이 1번");
     	return scanner.nextInt();
     }
+    
+    public static int inputPeopleNumber() {
+    	System.out.println("##예약할인원을입력하세요.");
+    	return scanner.nextInt();
+    }
 }
