4060e5009569164d1c74d45f1165599df120b8e8
racoco083 <uytr083@naver.com>
2019-04-16 16:08:28 +0900

    add UserMovie class

--- src/main/java/domain/UserMovie.java
+++ src/main/java/domain/UserMovie.java
@@ -1,4 +1,13 @@
 package domain;
 
 public class UserMovie {
+    private int movieId;
+    private int timeTableId;
+    private int peopleCount;
+
+    UserMovie(int movieId, int timeTableId, int peopleCount){
+        this.movieId = movieId;
+        this.timeTableId = timeTableId;
+        this.peopleCount = peopleCount;
+    }
 }
