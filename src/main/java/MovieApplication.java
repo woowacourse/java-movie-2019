1ea5e00197c2f1a166b4cf896fbae514ed518f1b
hyun <jhs695@naver.com>
2019-04-16 17:51:24 +0900

    Add isOverUser()
    초과 예매에 따른 오류사항 처리

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,7 +1,4 @@
-import domain.Movie;
-import domain.MovieRepository;
-import domain.Payment;
-import domain.PaymentInfo;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
@@ -13,27 +10,36 @@ public class MovieApplication {
     public static void main(String[] args) throws IOException {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
-
         List<PaymentInfo> paymentInfos = new ArrayList<>();
-
         boolean wantPay = false;
         while (!wantPay) {
             paymentInfos.add(selectMovie());
-
             wantPay = InputView.wantPay();
         }
         int payMoney = new Payment().pay(paymentInfos);
         OutputView.printPayResult(payMoney);
     }
 
+
     private static PaymentInfo selectMovie() throws IOException {
-        int movieId = InputView.inputMovieId();
+        try {
+            int movieId = InputView.inputMovieId();
+            Movie selectedMovie = MovieRepository.getMoviesById(movieId);
+            OutputView.printMovie(selectedMovie);
+            int movieTime = InputView.inputMovieTime() - 1;
+            int countOfUser = InputView.inputCountOfUser();
 
-        Movie selectedMovie = MovieRepository.getMoviesById(movieId);
-        OutputView.printMovie(selectedMovie);
-        int movieTime = InputView.inputMovieTime() - 1;
-        int countOfUser = InputView.inputCountOfUser();
+            isOverUser(selectedMovie , movieTime , countOfUser);
+            return new PaymentInfo(selectedMovie, movieTime, countOfUser);
+        }catch (IllegalArgumentException e){
+            return selectMovie();
+        }
+    }
 
-        return new PaymentInfo(selectedMovie , movieTime , countOfUser);
+    private static void isOverUser(Movie movie, int movieTime, int countOfuser){
+        if (!(movie.getPlaySchedules(movieTime).getCapacity() - countOfuser > 0)){
+            OutputView.overUser();
+            throw new IllegalArgumentException();
+        }
     }
 }
