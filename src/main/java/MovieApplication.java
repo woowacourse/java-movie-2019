15e099f4ca7a9de41c77f6e644a86fcd7bbe687b
Unknown <9105lgm@naver.com>
2019-04-16 16:10:01 +0900

    예약 할 영화 선택 기능 구현
    예약할 영화 선택,
    영화 없는 경우, 숫자 아닌 다른 것 왔을 경우에 대한 예외 처리

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,17 +1,45 @@
 import domain.Movie;
 import domain.MovieRepository;
+import domain.ScheduledMovie;
 import view.InputView;
 import view.OutputView;
 
+import java.sql.SQLSyntaxErrorException;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MovieApplication {
+
+    static List<Movie> books = new LinkedList<>();
+
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
 
-        int movieId = InputView.inputMovieId();
+        Movie movie = check(movies);
+        OutputView.printEachMovie(movie);
 
-        // TODO 구현 진행
     }
+
+    public static Movie check(List<Movie> movies){
+        try{
+            int movieId = InputView.inputMovieId();
+            return findMovie(movies,movieId);
+        }catch(IllegalArgumentException e){
+            return check(movies);
+        }
+    }
+
+    public static Movie findMovie(List<Movie> movies, int id){
+        for(Movie each : movies){
+            if(each.movieMatchOf(id)){
+                books.add(each);
+                return each;
+            }
+        }
+
+        throw new IllegalArgumentException("없는 영화표를 선택하셨습니다.");
+    }
+
+
 }
