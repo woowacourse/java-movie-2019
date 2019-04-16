b4cf023ac28b84255bc4aabe43569858c8bd0e75
yun <blpomspo@gmail.com>
2019-04-16 17:59:07 +0900

    추가 구현
    - 영화 예매 인원 입력
    - 결제 혹은 추가 예매 결정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,8 +15,23 @@ public class InputView {
         return scanner.nextInt();
     }
 
-    public static int inputMovietickets() {
+    public static int inputMovieTicket() {
         System.out.println("## 예약할 인원을 입력하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputContinueReserve() {
+        System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
+        return scanner.nextInt();
+    }
+
+    public static int inputPoint() {
+        System.out.println("## 포인트 사용 금액을 입력하세요. 포인트가 없으면 0 입력");
+        return scanner.nextInt();
+    }
+
+    public static int inputBillingType() {
+        System.out.println("## 신용카드는 1번, 현금은 2번");
         return scanner.nextInt();
     }
 }
