c533f42df7fc875ac3ef20b14ed3713265369fa4
LeeJoonkyu <ljk6463@gmail.com>
2019-04-16 14:31:58 +0900

    create README.md

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class OutputView {
     public static void printMovies(List<Movie> movies) {
+        System.out.println("## 상영 영화 목록");
         for (Movie movie : movies) {
             System.out.println(movie);
         }
