43c3d6e582e886a61a7f22810ef1ad0857e8683e
mrkwon <erased11@gmail.com>
2019-04-16 15:20:26 +0900

    최종 결제 결과를 위한 클래스 생성

---
+++ src/main/java/domain/PayInfo.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class PayInfo {
+    private int pointAmount;
+    private int payType;
+
+    public PayInfo(int pointAmount, int payType) {
+        this.pointAmount = pointAmount;
+        this.payType = payType;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
