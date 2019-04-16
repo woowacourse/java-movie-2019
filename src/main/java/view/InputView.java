f6f9dfcaf4023681a140d388e95d61584b504e67
JOngNan <dhorlawhddbs@naver.com>
2019-04-16 15:04:51 +0900

    예약할 영화 선택에서 숫자인지 판별기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,23 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    private static final String INPUT_MOVIE_ID_MSG = "## 예약할 영화를 선택하세요.";
+
+    private static final String ERROR_MSG_INPUT_MOVIE_ID = "숫자만 입력해주세요.";
+
     public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
+        try{
+            System.out.println(INPUT_MOVIE_ID_MSG);
+            return scanner.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println(ERROR_MSG_INPUT_MOVIE_ID);
+            scanner.next();
+            return  inputMovieId();
+        }
     }
 }
