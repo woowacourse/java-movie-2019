1c3fc053ccd7239aa5db60d94554c69f274878c5
starkim06 <starkim06@naver.com>
2019-04-16 15:56:14 +0900

    결제창 혹은 추가 진행 여부 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_MOVIE_TIME_MENT = "## 예약할 시간표를 선택하세요.(첫번째 사영 시간이 1번)";
     private static final String INPUT_NUMBER_OF_PEOPLE_MENT = "## 예약할 인원을 입력하세요.";
+    private static final String INPUT_PAY_OR_RESERVE_MENT = "## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번";
     private static final String INPUT_ERROR_MENT = "입력 형식이 올바르지 않습니다.";
 
     public static int inputMovieId() {
@@ -25,6 +26,15 @@ public class InputView {
     public static int inputNumberOfPeople() {
         try {
             System.out.println(INPUT_NUMBER_OF_PEOPLE_MENT);
+            return scanner.nextInt();
+        } catch (Exception e) {
+            throw new RuntimeException(INPUT_ERROR_MENT);
+        }
+    }
+
+    public static int inputPayOrReserve() {
+        try {
+            System.out.println(INPUT_PAY_OR_RESERVE_MENT);
             return scanner.nextInt();
         } catch (Exception e) {
             throw new RuntimeException(INPUT_ERROR_MENT);
