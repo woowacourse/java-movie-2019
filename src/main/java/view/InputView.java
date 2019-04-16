6f44cf099e74b1669d7d48d1aef1e4ca32fadd61
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-16 16:50:38 +0900

    [FIX] formatting

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
 			return inputMovieId();
 		}
 	}
-	
+
 	public static int inputStartDateTime() {
 		try {
 			System.out.println("## 예약할 시간표를  선택하세요.");
@@ -22,7 +22,7 @@ public class InputView {
 			return inputStartDateTime();
 		}
 	}
-	
+
 	public static int inputCapacity() {
 		try {
 			System.out.println("## 예약할 인원을 입력하세요.");
@@ -31,7 +31,16 @@ public class InputView {
 			return inputCapacity();
 		}
 	}
-	
+
+	public static int inputMoreBookingFlag() {
+		try {
+			System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
+			return Integer.parseInt(SCANNER.nextLine());
+		} catch (IllegalArgumentException e) {
+			return inputMoreBookingFlag();
+		}
+	}
+
 	public static int inputPoint() {
 		try {
 			System.out.println("## 결제를 진행합니다.");
@@ -41,7 +50,7 @@ public class InputView {
 			return inputPoint();
 		}
 	}
-	
+
 	public static int inputPaymentMethod() {
 		try {
 			System.out.println("## 신용카드는 1번, 현금은 2번");
