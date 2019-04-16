f51a60bbf087bc75bc153ba15a2c5e5b912a0599
Unknown <9105lgm@naver.com>
2019-04-16 17:09:27 +0900

    예약할 시간표 및 인원 입력

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -1,5 +1,6 @@
 import domain.Movie;
 import domain.MovieRepository;
+import domain.PlaySchedule;
 import domain.ScheduledMovie;
 import view.InputView;
 import view.OutputView;
@@ -12,12 +13,15 @@ public class MovieApplication {
 
     static List<Movie> books = new LinkedList<>();
 
+
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
 
         Movie movie = check(movies);
         OutputView.printEachMovie(movie);
+
+        PlaySchedule schedule = askSchedule(movie);
 
     }
 
@@ -26,6 +30,7 @@ public class MovieApplication {
             int movieId = InputView.inputMovieId();
             return findMovie(movies,movieId);
         }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
             return check(movies);
         }
     }
@@ -40,6 +45,20 @@ public class MovieApplication {
 
         throw new IllegalArgumentException("없는 영화표를 선택하셨습니다.");
     }
+
+    public static PlaySchedule askSchedule(Movie movie){
+        try {
+            int scheduleId = InputView.inputScheduleId() -1;
+            int people = InputView.inputPerson();
+
+            return movie.checkSchedue(scheduleId, people);
+
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return askSchedule(movie);
+        }
+    }
+
 
 
 }
