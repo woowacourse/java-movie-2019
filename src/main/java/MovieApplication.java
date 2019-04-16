ff61fca5b79aa5b3b879929ddca54ef4cd493a11
hyojaekim <hj00686@naver.com>
2019-04-16 14:41:47 +0900

    영화 정보 보여주기

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -3,15 +3,28 @@ import domain.MovieRepository;
 import view.InputView;
 import view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieApplication {
+    public static int getMovieId(int number){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(8);
+        return numbers.indexOf(number);
+    }
+
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
 
-        int movieId = InputView.inputMovieId();
+        int movieId = getMovieId(InputView.inputMovieId());
 
         // TODO 구현 진행
+        String sellectMovie = movies.get(movieId).toString();
+        System.out.println(sellectMovie);
     }
+
 }
