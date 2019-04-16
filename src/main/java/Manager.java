f7952b01d861534f3d367d6c55aa400cf535718f
racoco083 <uytr083@naver.com>
2019-04-16 15:20:06 +0900

    add startReserve method

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import domain.Movie;
+import domain.MovieRepository;
+import utils.MyUtil;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
 
 public class Manager {
+    private InputError inputError;
+    private MyUtil myUtil;
+    private List<Movie> movies;
+
+    Manager(){
+        inputError = new InputError();
+        myUtil = new MyUtil();
+    }
+
+    public void startReserve(int movieId){
+        this.movies = MovieRepository.getMovies();
+
+        while(true){
+            if(inputError.checkContainId(this.movies, movieId)) {
+                break;
+            }
+            movieId = InputView.inputMovieId();
+        }
+        OutputView.printIdMovie(movies, movieId);
+    }
 }
