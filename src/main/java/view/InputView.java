9ab16762456833df3f30b03f2075e772436566bf
Ko eunsuk <eunsukko.128@gmail.com>
2019-04-16 14:44:58 +0900

    Add input functions about reservation
    예약 관련 함수들 추가
    - 시간표 및 인원 선택

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,14 @@ public class InputView {
         System.out.println("## 예약할 영화를 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputMovieStartDateTimeIdx() {
+        System.out.println("## 예약할시간표를선택하세요.(첫번째상영시간이1번)");
+        return scanner.nextInt();
+    }
+
+    public static int inputNumReserve() {
+        System.out.println("## 예약할인원을입력하세요.");
+        return scanner.nextInt();
+    }
 }
