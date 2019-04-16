4c49004d7d9b01608c8803b1e32d37c7715e97e2
andole87 <andole87@gmail.com>
2019-04-16 14:55:41 +0900

    Create README.md
    애플리케이션 흐름 파악을 먼저 시도했다.
    필요한 기능 목록을 만들었다.

---
+++ src/test/java/BookMovieTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class BookMovieTest {
+
+    @Test
+    public void getSelectedMovieTest() {
+        BookMovie bookMovie = new BookMovie();
+        ByteArrayInputStream input = new ByteArrayInputStream("5".getBytes());
+        System.setIn(input);
+
+        assertEquals(bookMovie.movies.get(1), bookMovie.getSelectedMovie());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getSelectedMovieTest2() {
+        BookMovie bookMovie = new BookMovie();
+        ByteArrayInputStream input = new ByteArrayInputStream("2".getBytes());
+        System.setIn(input);
+
+        assertEquals(bookMovie.movies.get(1), bookMovie.getSelectedMovie());
+
+
+    }
+}
