707156adf940a3fa486c028eb516e54bf68cdd29
mrkwon <erased11@gmail.com>
2019-04-16 16:14:20 +0900

    예외 처리 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,27 +15,47 @@ public class InputView {
     }
 
     public static int inputMovieTime() {
-        System.out.println("## 예약할 시간표를 선택하세요.");
-        return scanner.nextInt();
+        try {
+            System.out.println("## 예약할 시간표를 선택하세요. (첫번째 상영 시간이 1번)");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            return inputMovieTime();
+        }
     }
 
     public static int inputReserveCount() {
-        System.out.println("## 예약할 인원을 입력하세요");
-        return scanner.nextInt();
+        try {
+            System.out.println("## 예약할 인원을 입력하세요");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            return inputReserveCount();
+        }
     }
 
     public static int inputChooseProcess() {
-        System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
-        return scanner.nextInt();
+        try {
+            System.out.println("## 예약을 종료하고 결제를 진행하려면 1번, 추가 예약을 진행하려면 2번");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            return inputChooseProcess();
+        }
     }
 
     public static int inputPointAmount() {
-        System.out.println("## 포인트 사용 금액을 입력하세요. 포인트가 없으면 0 입력");
-        return scanner.nextInt();
+        try {
+            System.out.println("## 포인트 사용 금액을 입력하세요. 포인트가 없으면 0 입력");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            return inputPointAmount();
+        }
     }
 
     public static int inputPayType() {
-        System.out.println("## 신용카드는 1번, 현금은 2번");
-        return scanner.nextInt();
+        try {
+            System.out.println("## 신용카드는 1번, 현금은 2번");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (IllegalArgumentException e) {
+            return inputPayType();
+        }
     }
 }
