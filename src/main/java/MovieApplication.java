f7fa1abdbe22505b16e8e41ccadaab45fb92b12a
JINSIL <kjin_048@gmail.com>
2019-04-16 18:05:24 +0900

    Updata MovieApplication 기능 구현

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -12,6 +12,37 @@ public class MovieApplication {
 
         int movieId = InputView.inputMovieId();
 
-        // TODO 구현 진행
+        Movie movie = MovieRepository.getMovieInformation(movieId);
+        OutputView.printMovie(movie);
+
+        int moviewTime = InputView.inputMovieTime();
+        int movieNum = InputView.inputMovieNum();
+        int InputNextStep = InputView.inputNextStep();
+
+        // 예약내역
+        System.out.println(movie.numberMovie(moviewTime, movieNum));
+        movieprice = movie.getPrice() * movieNum;
+
+
+        // 결제 진행
+        OutputView.outputPaymentStep();
+
+        //포인트
+        int point = InputView.inputMoviePoint();
+
+        //신용카드
+        int cardcash = InputView.inputCardorCash();
+
+        if(cardcash ==1) { //card
+            movieprice -= ((int)(movieprice * 0.05)) ;
+        }
+        if(cardcash ==2) { //cash
+            movieprice -= ((int)(movieprice * 0.02)) ;
+        }
+
+        movieprice -= point;
+
+        System.out.println("최종결제한금액은 : " + movieprice + "입니다.");
+        OutputView.outputconfirm();
     }
-}
+} ////
