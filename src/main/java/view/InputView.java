6bcb134582b86c36a5b81c97c1590e2ab0e1bccc
imkimheejoo <jujudamdadi@naver.com>
2019-04-16 15:46:41 +0900

    6.추가예약유무를 입력받는 함수 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -60,6 +60,15 @@ public class InputView {
             return inputMember();
         }
     }
+    public static int inputAddReservation(){
+        try {
+            System.out.println("## 예약을 종료하고 결제를 진행하려면 1번,추가 예약을 진행하려면 2번");
+            return isOneOrTwo(intException());
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputAddReservation();
+        }
+    }
    public static int isOneOrTwo(int input){
         if(!(input==1 || input==2)){
             throw new IllegalArgumentException("입력은 1또는 2를 입력해 주세요.");
