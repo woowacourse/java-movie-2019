e50090e9e6d0ddab117ecf666d0fb29bbed81ebc
starhn87 <starhn87@gmail.com>
2019-04-16 17:36:09 +0900

    예약할 영화 선택

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,19 @@
 package view;
 
+import domain.Movie;
+import domain.PlaySchedule;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    /*
+     * 예약할 영화를 선택
+     */
     public static int inputMovieId() {
         System.out.println("## 예약할 영화를 선택하세요.");
         return scanner.nextInt();
