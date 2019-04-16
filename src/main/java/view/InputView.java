ff78c1212aa394b54c29a47e2cae586fd5677c41
imkimheejoo <jujudamdadi@naver.com>
2019-04-16 15:26:48 +0900

    4. 예약할 시간표를 입력하는 함수 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -36,10 +36,20 @@ public class InputView {
         throw new IllegalArgumentException("상영하고 있는 영화의 id를 입력해주세요. ");
     }
 
+    public static int inputMovieTime(Movie choiceMovie){
+        try {
+            System.out.println("## 예약할 시간표를 선택하세요.(첫번째 상영시간이 1번)");
+            return isContainTime(choiceMovie,intException());
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return inputMovieTime(choiceMovie);
+        }
+    }
     public static int isContainTime(Movie movie,int inputTime){
         if(movie.getPlaySchedulesLength()<inputTime || inputTime<0){
             throw new IllegalArgumentException("올바른 상영시간표를 선택해 주세요.");
         }
         return inputTime;
     }
+
 }
