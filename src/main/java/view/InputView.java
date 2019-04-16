e2b45e83d8509df3ef9a6273789742dd6a8be120
starkim06 <starkim06@naver.com>
2019-04-16 15:36:29 +0900

    사용자로부터 예약할 인원 입력 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_MOVIE_TIME_MENT = "## 예약할 시간표를 선택하세요.(첫번째 사영 시간이 1번)";
+    private static final String INPUT_NUMBER_OF_PEOPLE_MENT = "## 예약할 인원을 입력하세요.";
     private static final String INPUT_ERROR_MENT = "입력 형식이 올바르지 않습니다.";
 
     public static int inputMovieId() {
@@ -15,6 +16,15 @@ public class InputView {
     public static int inputMovieTime() {
         try {
             System.out.println(INPUT_MOVIE_TIME_MENT);
+            return scanner.nextInt();
+        } catch (Exception e) {
+            throw new RuntimeException(INPUT_ERROR_MENT);
+        }
+    }
+
+    public static int inputNumberOfPeople() {
+        try {
+            System.out.println(INPUT_NUMBER_OF_PEOPLE_MENT);
             return scanner.nextInt();
         } catch (Exception e) {
             throw new RuntimeException(INPUT_ERROR_MENT);
