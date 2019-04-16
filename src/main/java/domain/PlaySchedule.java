9aac02d3c22871e51dabee5a8e6875c6e7d9bb09
mrkwon <erased11@gmail.com>
2019-04-16 15:13:22 +0900

    예약 내역 출력시 string 처리를 위한 toStringExceptCapacity 메서드 구현

--- src/main/java/domain/PlaySchedule.java
+++ src/main/java/domain/PlaySchedule.java
@@ -13,6 +13,10 @@ public class PlaySchedule {
         this.capacity = capacity;
     }
 
+    public String toStringExceptCapacity() {
+        return "시작시간: " + format(startDateTime) + "\n";
+    }
+
     @Override
     public String toString() {
         return "시작시간: " + format(startDateTime) + " 예약가능인원: " + capacity + "\n";
