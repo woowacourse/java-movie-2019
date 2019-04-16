a278983e5ef664277f63f028cdba81619301114a
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-04-16 14:21:31 +0900

    [추가] 정수 입력 받는 기능 InputUtil클래스

---
+++ src/main/java/utils/InputUtil.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.util.Scanner;
+
+public class InputUtil {
+    private final static Scanner SCANNER = new Scanner(System.in);
+
+    public static int getInt() {
+        try {
+            return Integer.parseInt(getString());
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 입력하세요");
+            return getInt();
+        }
+    }
+    private static String getString() {
+        return SCANNER.nextLine();
+    }
+}
