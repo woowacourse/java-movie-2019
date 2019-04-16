e4fa5ed8077020fe6170e2f97ae2a0ac673dbfce
Soojin Roh <sooreal91@gmail.com>
2019-04-16 14:59:22 +0900

    영화 선택 기능 메서드로 분리
    영화를 선택하는 코드를 selectMovie라는 함수로 분리

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -6,28 +6,38 @@ import view.OutputView;
 import java.util.List;
 
 public class MovieApplication {
+	
+	private Movie selectMovie(List<Movie> movies) {
+        int movieId = 0;
+        try {
+        	movieId = InputView.inputMovieId();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("1 ~ 4 사이의 영화를 선택하세요.");
+            movieId = InputView.inputMovieId();
+        }
+    	System.out.println(movies.get(movieId - 1));
+    	
+        return movies.get(movieId - 1);
+	}
+	
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
-        int movieId = 0;
+        
+
         int movieTime = 0;
-        try {
-            movieId = InputView.inputMovieId();
-            System.out.println(movies.get(movieId - 1));
-        } catch (IndexOutOfBoundsException e) {
-        	System.out.println("1 ~ 4 사이의 영화를 선택하세요.");
-        	movieId = InputView.inputMovieId();
-            System.out.println(movies.get(movieId - 1));
-        }
+ 
         
         try {
             movieTime = InputView.inputMovieTime();
-            System.out.println(movies.get(movieId).getPlayMovieSchedule(movieTime - 1));
+            System.out.println(movies.get(movieId - 1).getPlayMovieSchedule(movieTime - 1));
+            
         } catch (IndexOutOfBoundsException e) {
         	System.out.println("상영시간을 확인 후 다시 선택해주세요");
             movieTime = InputView.inputMovieTime();
-            System.out.println(movies.get(movieId).getPlayMovieSchedule(movieTime - 1));
+            System.out.println(movies.get(movieId - 1).getPlayMovieSchedule(movieTime - 1));
         }
 
+        
     }
 }
