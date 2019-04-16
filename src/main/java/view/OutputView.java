e1be02e3ac4cef94594dd78b50bdf260b57adb8d
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-16 14:47:01 +0900

    [MOD] format 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,9 +5,9 @@ import domain.Movie;
 import java.util.List;
 
 public class OutputView {
-    public static void printMovies(List<Movie> movies) {
-        for (Movie movie : movies) {
-            System.out.println(movie);
-        }
-    }
+	public static void printMovies(List<Movie> movies) {
+		for (Movie movie : movies) {
+			System.out.println(movie);
+		}
+	}
 }
