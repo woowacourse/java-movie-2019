d9436a1ba531b6f4b0cfe3c40c28b89f3713383f
mrkwon <erased11@gmail.com>
2019-04-16 16:25:11 +0900

    인스턴스 변수에 reservationTimeList 추가, getCountByMovie 메서드와 getTimeByMovie 메서드 추

--- src/main/java/domain/ReservationList.java
+++ src/main/java/domain/ReservationList.java
@@ -2,18 +2,33 @@ package domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ReservationList {
-    private Map<Movie, Integer> reservationList = new HashMap<>();
+    private Map<Movie, Integer> reservationCountList = new HashMap<>();
+    private Map<Movie, Integer> reservationTimeList = new HashMap<>();
 
-    public void add(Movie movie, Integer num) {
-        reservationList.put(movie, num);
+    public void add(Movie movie, Integer count, Integer timeId) {
+        reservationCountList.put(movie, count);
+        reservationTimeList.put(movie, timeId);
+    }
+
+    public Set<Movie> getMovieSet() {
+        return this.reservationCountList.keySet();
+    }
+
+    public Integer getCountByMovie(Movie movie) {
+        return reservationCountList.get(movie);
+    }
+
+    public Integer getTimeByMovie(Movie movie) {
+        return reservationTimeList.get(movie);
     }
 
     int getTotalPrice() {
         int totalPrice = 0;
-        for (Movie movie : reservationList.keySet()) {
-            int eachPrice = movie.getPrice() * reservationList.get(movie);
+        for (Movie movie : reservationCountList.keySet()) {
+            int eachPrice = movie.getPrice() * reservationCountList.get(movie);
             totalPrice += eachPrice;
         }
         return totalPrice;
