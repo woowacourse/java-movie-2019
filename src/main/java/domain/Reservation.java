21fc00be7e09cea6fbb783b5210da32bc5e97a77
je941004 <je941004@naver.com>
2019-04-16 18:06:19 +0900

    코드 컨벤션 수정

--- src/main/java/domain/Reservation.java
+++ src/main/java/domain/Reservation.java
@@ -1,37 +1,36 @@
 package domain;
 
 public class Reservation {
-        private static final char NEW_LINE = '\n';
+    private static final char NEW_LINE = '\n';
 
-        private final Movie movie;
-        private final int personnel;
-        private final int scheduleNo;
+    private final Movie movie;
+    private final int personnel;
+    private final int scheduleNo;
 
-        public Reservation(Movie movie, int personnel, int scheduleNo) {
-                if(!movie.modifyScheduleCapacity(scheduleNo, personnel)){
-                        throw new IllegalArgumentException("인원이 너무 많습니다.");
-                }
-                this.movie = movie;
-                this.personnel = personnel;
-                this.scheduleNo = scheduleNo;
+    public Reservation(Movie movie, int personnel, int scheduleNo) {
+        if(!movie.modifyScheduleCapacity(scheduleNo, personnel)){
+            throw new IllegalArgumentException("인원이 너무 많습니다.");
         }
+        this.movie = movie;
+        this.personnel = personnel;
+        this.scheduleNo = scheduleNo;
+    }
 
-        public PlaySchedule getPlaySchedule(){
-                return movie.getSchedule(scheduleNo);
-        }
+    public PlaySchedule getPlaySchedule(){
+        return movie.getSchedule(scheduleNo);
+    }
 
-        public long getTotalMoney(){
-                return this.personnel * movie.getPrice();
-        }
+    public long getTotalMoney(){
+        return this.personnel * movie.getPrice();
+    }
 
-        @Override
-        public String toString(){
-                StringBuilder sb = new StringBuilder();
-                sb.append(movie.printMovie());
-                sb.append( movie.getSchedule(scheduleNo).printSchedule());
-                sb.append("예약인원: "+personnel+"명");
-                return sb.toString();
-
-        }
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(movie.printMovie());
+        sb.append( movie.getSchedule(scheduleNo).printSchedule());
+        sb.append("예약인원: "+personnel+"명");
+        return sb.toString();
+    }
 
 }
