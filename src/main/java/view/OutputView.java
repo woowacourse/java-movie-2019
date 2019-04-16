0445b296328b4b7f939e94824f4ae4795ca799be
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-16 15:14:32 +0900

    [ADD] 입력받은 영화 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import domain.Movie;
+import domain.MovieRepository;
 
 import java.util.List;
 
@@ -9,5 +10,15 @@ public class OutputView {
 		for (Movie movie : movies) {
 			System.out.println(movie);
 		}
+	}
+
+	// TODO indent 줄이기
+	public static void printMovie(int movieId) {
+		for(Movie movie : MovieRepository.getMovies()) {
+			if(movie.getId() == movieId) {
+				System.out.println(movie);
+			}
+		}
+		
 	}
 }
