e5b98e4a7f343c2fc1b490e8304c7e8cc6260db9
483759 <483759@naver.com>
2019-04-16 15:02:03 +0900

    예약할 영화 번호가 상영 목록 범위 내에 있는지 검증하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,32 @@
 package view;
 
+import domain.Movie;
+import domain.MovieRepository;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static boolean IsCorrectMovieId(int movieId) {
+    List<Movie> movies = MovieRepository.getMovies();
+    for (Movie movie : movies) {
+      if (movie.IsCorrectNumberOfMovie(movieId)) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  public static int inputMovieId() {
+    int movieId;
+    do {
+      System.out.println("## 예약할 영화를 선택하세요.");
+      movieId = scanner.nextInt();
+    }while(!IsCorrectMovieId(movieId));
+    return movieId;
+  }
+
 }
