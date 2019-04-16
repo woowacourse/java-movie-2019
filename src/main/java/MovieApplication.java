0402e4cc32b4f4351feedfee4c88a64632410ff4
HeavyRyan92 <heavyryan92@gmail.com>
2019-04-16 14:41:30 +0900

    movie
     - 입력한 예약 영화 번호를 출력하는 기능을 추가합니다.

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -5,13 +5,25 @@ import view.OutputView;
 
 import java.util.List;
 
+import static domain.MovieRepository.getMovies;
+
 public class MovieApplication {
+
+    private final int SET_MOVIE_LOCATION = 1;
+
+    private void printMovieList(int movieId){
+
+        System.out.println(getMovies().get(movieId - SET_MOVIE_LOCATION));
+    }
     public static void main(String[] args) {
-        List<Movie> movies = MovieRepository.getMovies();
+        List<Movie> movies = getMovies();
         OutputView.printMovies(movies);
+        MovieApplication movieCompany = new MovieApplication();
 
         int movieId = InputView.inputMovieId();
+        movieCompany.printMovieList(movieId);
 
         // TODO 구현 진행
     }
+
 }
