406de224c2fac18553a226be602ea29a28e11a88
Yujin Kim <99stella24@naver.com>
2019-04-16 14:46:40 +0900

    add method getId

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 - class MovieRespository
 
     - getMovie() : 특정 id를 입력 받으면 그 영화의 정보만 반환하는 함수
+
+- class Movie
+
+    - getId() : movie의 id를 반환하는 함수
--- src/main/java/domain/MovieRepository.java
+++ src/main/java/domain/MovieRepository.java
@@ -6,48 +6,51 @@ import java.util.List;
 import static utils.DateTimeUtils.createDateTime;
 
 public class MovieRepository {
-    private static List<Movie> movies = new ArrayList<>();
 
-    static {
-        Movie movie1 = new Movie(1, "생일", 8_000);
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 12:00"), 6));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 14:40"), 6));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 17:00"), 6));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 19:40"), 3));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 22:00"), 3));
-        movies.add(movie1);
+  private static List<Movie> movies = new ArrayList<>();
 
-        Movie movie2 = new Movie(5, "돈", 10_000);
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 08:00"), 3));
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 10:30"), 5));
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 13:00"), 5));
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 15:30"), 5));
-        movies.add(movie2);
+  static {
+    Movie movie1 = new Movie(1, "생일", 8_000);
+    movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 12:00"), 6));
+    movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 14:40"), 6));
+    movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 17:00"), 6));
+    movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 19:40"), 3));
+    movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 22:00"), 3));
+    movies.add(movie1);
 
-        Movie movie3 = new Movie(7, "파이브피트", 9_000);
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 13:00"), 4));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 15:40"), 4));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 18:00"), 4));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 20:40"), 3));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 23:15"), 3));
-        movies.add(movie3);
+    Movie movie2 = new Movie(5, "돈", 10_000);
+    movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 08:00"), 3));
+    movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 10:30"), 5));
+    movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 13:00"), 5));
+    movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 15:30"), 5));
+    movies.add(movie2);
 
-        Movie movie4 = new Movie(8, "덤보", 9_000);
-        movie4.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 11:30"), 2));
-        movie4.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 16:00"), 3));
-        movie4.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 21:30"), 2));
-        movies.add(movie4);
+    Movie movie3 = new Movie(7, "파이브피트", 9_000);
+    movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 13:00"), 4));
+    movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 15:40"), 4));
+    movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 18:00"), 4));
+    movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 20:40"), 3));
+    movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 23:15"), 3));
+    movies.add(movie3);
+
+    Movie movie4 = new Movie(8, "덤보", 9_000);
+    movie4.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 11:30"), 2));
+    movie4.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 16:00"), 3));
+    movie4.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 21:30"), 2));
+    movies.add(movie4);
+  }
+
+  public static List<Movie> getMovies() {
+    return movies;
+  }
+
+  public static Movie getMovie(int id) {
+    for (Movie movie : movies) {
+      if (movie.getId() == id) {
+        return movie;
+      }
     }
-
-    public static List<Movie> getMovies() {
-        return movies;
-    }
-
-    public static Movie getMovie(int id) {
-        for(Movie movie : movies){
-            if(movie.getId() == id) return movie;
-            }
-        return null;
-        }
-    }
+    return null;
+  }
 }
+
