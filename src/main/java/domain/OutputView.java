cc4979f39114b460ba27beed4b198bf18bb0b370
JINSIL <kjin_048@gmail.com>
2019-04-16 17:53:55 +0900

    Add OutPutView Class 생성 및 기능 구현

---
+++ src/main/java/domain/OutputView.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.List;
+
+public class OutputView { public static void printList() { System.out.println("## 상영 영화 목록"); } public static void printMovies(List movies) { for (Movie movie : movies) { System.out.println(movie); } }
+
+    public static void printMovie(Movie movie) {
+        System.out.println(movie);
+    }
+
+    public static void outputPaymentStep() {
+        System.out.println("## 결제를진행합니다");
+    }
+
+    public static void outputconfirm() {
+        System.out.println("예매를 완료했습니다. 즐거운 영화 관람되세요");
+    }
+}
