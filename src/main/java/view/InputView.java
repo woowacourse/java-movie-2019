ad7a63b69b0c3a1263e16bc115b8bbcbae1236f6
bactoria <bactoria@gmail.com>
2019-04-16 17:25:23 +0900

    FEAT :: 스키쥴번호, 예약 인원 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,4 +9,15 @@ public class InputView {
         System.out.println("## 예약할 영화를 선택하세요.");
         return scanner.nextInt();
     }
+
+    public static int inputPlaySchedule() {
+        System.out.println("## 예약할 시간표를 선택하세요.(첫번째 상영 시간이 1번");
+        return scanner.nextInt();
+    }
+
+    public static int inputMovieCapacity() {
+        System.out.println("## 예약할 인원을 입력하세요.");
+        return scanner.nextInt();
+    }
+
 }
