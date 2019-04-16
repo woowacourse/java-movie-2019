5465da856fdf117dc7ba74014be82e44bdfb0f22
SUNGBUMPARK <psbum1609@gmail.com>
2019-04-16 16:56:32 +0900

    MovieApplication 클래스 리팩토링

--- src/main/java/MovieApplication.java
+++ src/main/java/MovieApplication.java
@@ -8,40 +8,48 @@ import java.util.List;
 
 public class MovieApplication {
 
+    private static List<ReservingMovie> reservedMovies = new ArrayList<>();
+
     public static void main(String[] args) {
         List<Movie> movies = MovieRepository.getMovies();
-        List<ReservingMovie> reservedMovies = new ArrayList<>();
         OutputView.printMovies(movies);
 
         boolean reservingProcess = true;
-        while(reservingProcess) {
+        while (reservingProcess) {
             int movieId = InputView.inputMovieId();
-
-            // TODO 구현 진행
             Movie selectedMovie = MovieRepository.getMovieUsingMovieID(movieId);
             OutputView.printSelectedMovie(selectedMovie);
-
-            int moveStartTimeNumber = InputView.inputMovieStartTime();
-            int numberOfPeople = InputView.inputNumberOfPeople();
+            addReservedMovie(selectedMovie);
             reservingProcess = (InputView.inputPaymentOrAddingReserve() == 2)
                     ? true : false;
-            reservedMovies.add(new ReservingMovie(selectedMovie, moveStartTimeNumber, numberOfPeople));
         }
-
         OutputView.printDetailsOfReservingMovie(reservedMovies);
+        startPayment(reservedMovies);
+    }
+
+    private static void addReservedMovie(Movie selectedMovie) {
+        int moveStartTimeNumber = InputView.inputMovieStartTime();
+        int numberOfPeople = InputView.inputNumberOfPeople();
+        reservedMovies.add(new ReservingMovie(selectedMovie, moveStartTimeNumber, numberOfPeople));
+    }
+
+    private static int calculateTotalMoviePrice(List<ReservingMovie> reservedMovies) {
+        int totalMoviePrice = 0;
+        for (ReservingMovie reservedMovie : reservedMovies) {
+            totalMoviePrice += reservedMovie.getPrice();
+        }
+        return totalMoviePrice;
+    }
+
+    private static void startPayment(List<ReservingMovie> reservedMovies) {
         int inputPoint = InputView.inputPoint();
         Point point = new Point(inputPoint);
         int paymentMethod = InputView.inputPaymentMethod();
 
-        int totalMoviePrice = 0;
-        for(ReservingMovie reservedMovie : reservedMovies) {
-            totalMoviePrice += reservedMovie.getPrice();
-        }
+        int totalMoviePrice = calculateTotalMoviePrice(reservedMovies);
 
         Pay pay = new Pay(point, totalMoviePrice);
         OutputView.printResultPrice(pay, paymentMethod);
-
     }
-
 
 }
