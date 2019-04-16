7575f5ecf6c9b71a3f334cc6507f3a11e48dd6c1
ep1stas1s <masloz@konkuk.ac.kr>
2019-04-16 16:19:56 +0900

    [MOD] 예매한 영화 출력 메소드 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import domain.Movie;
 import domain.PlaySchedule;
+import domain.Reservation;
 
 public class OutputView {
 	public static void printMovies(List<Movie> movies) {
@@ -15,18 +16,15 @@ public class OutputView {
 	public static void printMovie(Movie movie) {
 		System.out.println(movie);
 	}
-	
+
 	public static void printPlaySchedule(PlaySchedule playSchedule) {
 		System.out.println(playSchedule);
 	}
-	
-	public static void printBookingResult(Movie movie, PlaySchedule playSchedule, int capacity) {
-		System.out.println("예약 내역");
-		System.out.println(movie);
-		System.out.println(playSchedule);
-		System.out.println("예약 인원 : " + capacity);
+
+	public static void printAllReservation(List<Reservation> reservations) {
+		reservations.forEach(reservation -> System.out.println(reservation));
 	}
-	
+
 	public static void printBookingFinishMessage() {
 		System.out.println("예매를 완료했습니다. 즐거운 영화 관람 되세요.");
 	}
