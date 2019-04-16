6b87624183cacae6f5d2a30b732e5e72f5759c40
hyperpace <cnk9109@gmail.com>
2019-04-16 14:36:36 +0900

    Add NaturalNumber class

---
+++ src/main/java/utils/NaturalNumber.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class NaturalNumber {
+	private int number;
+
+	public NaturalNumber(int number) {
+		if (number < 1) {
+			throw new IllegalArgumentException("1이상의 수만 입력 가능합니다.");
+		}
+
+		this.number = number;
+	}
+
+	public int getNumber() {
+		return this.number;
+	}
+}
