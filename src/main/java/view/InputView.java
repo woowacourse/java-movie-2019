14643729b571a50b64648be26e337018651b217b
heebong <heebg2@naver.com>
2019-04-16 14:50:05 +0900

    영화 선택시 숫자만 입력하게 예외 추가 InputView 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,12 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
+        try{
+            System.out.println("## 예약할 영화를 선택하세요.");
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e){
+            System.out.println("숫자만 입력해주세요");
+            return inputMovieId();
+        }
     }
 }
