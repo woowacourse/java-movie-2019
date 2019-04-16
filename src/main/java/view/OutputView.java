eaa56aa33506a3ce97b047b335bb6c895480fce3
imkimheejoo <jujudamdadi@naver.com>
2019-04-16 18:09:42 +0900

    공백제거

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -12,17 +12,14 @@ public class OutputView {
             System.out.println(movie);
         }
     }
-
     public static void printReservation(List<ChoiceMovie> choiceMovies) {
         for (ChoiceMovie choice : choiceMovies) {
             System.out.println(choice.toString());
         }
     }
-
     public static void printTotalMoney(Money money) {
         System.out.println(money.toString());
         System.out.println("예매를 완료했습니다. 즐거운 영화 관람되세요.");
     }
-
 
 }
