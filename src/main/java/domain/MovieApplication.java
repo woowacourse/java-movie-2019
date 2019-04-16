400bdc7f02d855796b5d3f7740b8e441e8e251aa
DiceMono <qrdfg@naver.com>
2019-04-16 15:19:31 +0900

    기능 : 예약할 영화 선택 구현

--- src/main/java/domain/MovieApplication.java
+++ src/main/java/domain/MovieApplication.java
@@ -1,9 +1,13 @@
 package domain;
 
+import java.util.InputMismatchException;
+
+import view.InputView;
 import view.OutputView;
 
 public class MovieApplication {
 	public static void main(String[] args) {
 		OutputView.printMovies(MovieRepository.getMovies());
+		OutputView.printMovie(InputView.inputMovieId());
 	}
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,14 +1,19 @@
 package view;
 
 import domain.Movie;
+import domain.MovieRepository;
 
 import java.util.List;
 
 public class OutputView {
-	private static String MOVIE_LIST_MESSAGE = "## 상영 영화 목록";
+	
+	public static void printMovie(int movieId) {
+		Movie movie = MovieRepository.getMovie(movieId); 
+		System.out.println(movie);
+	}
     
 	public static void printMovies(List<Movie> movies) {
-    	System.out.println(MOVIE_LIST_MESSAGE);
+    	System.out.println("## 상영 영화 목록");
         
     	for (Movie movie : movies) {
             System.out.println(movie);
